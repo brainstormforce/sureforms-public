9259765e08db590f19dfe866b2c5561e6a93d660
Abhishek Rijal <avee.ws@gmail.com>
2023-03-01 08:31:47 +0545

    stubs file updated

--- tests/php/stubs/sf-stubs.php
+++ tests/php/stubs/sf-stubs.php
@@ -57,6 +57,92 @@ namespace SureForms {
         }
     }
 }
+namespace SureForms\Inc\Blocks {
+    /**
+     * Block base class.
+     */
+    abstract class Base
+    {
+        /**
+         * Optional directory to .json block data files.
+         *
+         * @var string
+         * @since X.X.X
+         */
+        protected $directory = '';
+        /**
+         * Holds the block.
+         *
+         * @var object
+         * @since X.X.X
+         */
+        protected $block;
+        /**
+         * Register the block for dynamic output
+         *
+         * @return void
+         * @since X.X.X
+         */
+        public function register()
+        {
+        }
+        /**
+         * Get the called class directory path
+         *
+         * @return string
+         * @since X.X.X
+         */
+        public function get_dir()
+        {
+        }
+        /**
+         * Optionally run a function to modify attributes before rendering.
+         *
+         * @param array  $attributes Block attributes.
+         * @param string $content   Post content.
+         * @param array  $block Block attributes.
+         *
+         * @return function
+         * @since X.X.X
+         */
+        public function pre_render($attributes, $content, $block)
+        {
+        }
+        /**
+         * Run any block middleware before rendering.
+         *
+         * @param array  $attributes Block attributes.
+         * @param string $content   Post content.
+         * @return boolean|\WP_Error;
+         * @since X.X.X
+         */
+        protected function middleware($attributes, $content)
+        {
+        }
+        /**
+         * Allows filtering of attributes before rendering.
+         *
+         * @param array $attributes Block attributes.
+         * @return array $attributes
+         * @since X.X.X
+         */
+        public function get_attributes($attributes)
+        {
+        }
+        /**
+         * Render the block
+         *
+         * @param array  $attributes Block attributes.
+         * @param string $content Post content.
+         *
+         * @return string
+         * @since X.X.X
+         */
+        public function render($attributes, $content)
+        {
+        }
+    }
+}
 namespace SureForms\Inc\Traits {
     /**
      * Trait Get_Instance.
@@ -108,6 +194,71 @@ namespace SureForms\Inc {
         }
     }
 }
+namespace SureForms\Admin {
+    /**
+     * Gutenberg hooks handler class.
+     *
+     * @since X.X.X
+     */
+    class Gutenberg_Hooks
+    {
+        use \SureForms\Inc\Traits\Get_Instance;
+        /**
+         * Class constructor.
+         *
+         * @return void
+         * @since X.X.X
+         */
+        public function __construct()
+        {
+        }
+        /**
+         * Add Editor Scripts.
+         *
+         * @return void
+         * @since X.X.X
+         */
+        public function add_editor_assets()
+        {
+        }
+    }
+    /**
+     * Admin handler class.
+     *
+     * @since X.X.X
+     */
+    class Admin
+    {
+        use \SureForms\Inc\Traits\Get_Instance;
+        /**
+         * Class constructor.
+         *
+         * @return void
+         * @since X.X.X
+         */
+        public function __construct()
+        {
+        }
+        /**
+         * Add menu page.
+         *
+         * @return void
+         * @since X.X.X
+         */
+        public function add_menu_page()
+        {
+        }
+        /**
+         * Enqueue Admin Scripts.
+         *
+         * @return void
+         * @since X.X.X
+         */
+        public function enqueue_scripts()
+        {
+        }
+    }
+}
 namespace {
     /**
      * Plugin Name: SureForms
@@ -127,7 +278,8 @@ namespace {
     \define('SUREFORMS_BASE', \plugin_basename(\SUREFORMS_FILE));
     \define('SUREFORMS_DIR', \plugin_dir_path(\SUREFORMS_FILE));
     \define('SUREFORMS_URL', \plugins_url('/', \SUREFORMS_FILE));
-    \define('SUREFORMS_VER', 'X.X.X');
+    \define('SUREFORMS_VER', '0.0.1');
     // ------ ADDITIONAL CONSTANTS ------- //
     \define('SUREFORMS_FORMS_POST_TYPE', 'sureforms_form');
+    \define('SUREFORMS_ENTRIES_POST_TYPE', 'sureforms_entry');
 }
