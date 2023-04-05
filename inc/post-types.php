ce890229eafa2b84e1e87f8ce3a258c7c2ce8037
Vansh Kapoor <67928850+vanshk141999@users.noreply.github.com>
2023-04-05 13:51:25 +0530

    SF-7 Added Column That Display Shortcode (#13)

--- inc/post-types.php
+++ inc/post-types.php
@@ -25,6 +25,11 @@ class Post_Types {
 	public function __construct() {
 		add_action( 'init', [ $this, 'register_post_types' ] );
 		add_filter( 'template_include', [ $this, 'page_template' ], PHP_INT_MAX );
+
+		add_filter( 'manage_sureforms_form_posts_columns', [ $this, 'custom_sureforms_form_columns' ] );
+		add_action( 'manage_sureforms_form_posts_custom_column', [ $this, 'custom_sureforms_form_column_data' ], 10, 2 );
+		add_shortcode( 'sureforms', [ $this, 'sureforms_shortcode' ] );
+
 	}
 
 	/**
@@ -119,8 +124,70 @@ class Post_Types {
 		if ( is_singular( SUREFORMS_FORMS_POST_TYPE ) ) {
 			$file_name = 'single-form.php';
 			$template  = locate_template( $file_name ) ? locate_template( $file_name ) : SUREFORMS_DIR . '/templates/' . $file_name;
-			$template  = apply_filters('sureforms_form_template', $template);
+			$template  = apply_filters( 'sureforms_form_template', $template );
 		}
 		return $template;
+	}
+
+	/**
+	 * Custom Shortcode.
+	 *
+	 * @param array $atts Attributes.
+	 * @return $content Post Content.
+	 */
+	public function sureforms_shortcode( $atts ) {
+		$atts = shortcode_atts(
+			array(
+				'id'    => '',
+				'title' => '',
+			),
+			$atts
+		);
+
+		$id    = $atts['id'];
+		$title = $atts['title'];
+
+		$post = get_post( $id );
+
+		if ( $post ) {
+			$content = $post->post_content;
+			return $content;
+		}
+
+	}
+
+	/**
+	 * Add custom column header.
+	 *
+	 * @param array $columns Attributes.
+	 * @return array $columns Post Content.
+	 */
+	public function custom_sureforms_form_columns( $columns ) {
+		$columns = array(
+			'cb'        => $columns['cb'],
+			'title'     => $columns['title'],
+			'sureforms' => __( 'Shortcode', 'sureforms' ),
+			'author'    => $columns['author'],
+			'date'      => $columns['date'],
+		);
+		return $columns;
+	}
+
+	/**
+	 * Populate custom column with data.
+	 *
+	 * @param string  $column Attributes.
+	 * @param integer $post_id Attributes.
+	 */
+	public function custom_sureforms_form_column_data( $column, $post_id ) {
+		$post_title = esc_html( get_the_title( $post_id ) );
+		if ( 'sureforms' === $column ) {
+			ob_start();
+			?>
+			<input type="text" readonly value="[sureforms id='<?php echo esc_html( $post_id ); ?>' title='<?php echo esc_html( $post_title ); ?>']" onClick="this.select();" style="cursor: pointer; width: -webkit-fill-available; border: none; background: transparent; font-family: Consolas,Monaco,monospace;
+			" />
+			<?php
+			ob_end_flush();
+		}
 	}
 }
