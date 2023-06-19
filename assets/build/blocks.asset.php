b2b7fd73e740f43487fa6c611f9cc8631e338cb2
Vansh Kapoor <67928850+vanshk141999@users.noreply.github.com>
2023-06-19 13:28:46 +0530

    Fix:SF-88-Settings Tab Update Fix (#49)
    * SF-50 initial commit
    
    * phpstan fix
    
    * phpcs fix
    
    * SF-88 initial commit
    
    * SF-88 lint-js fix

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,2 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '49e24709f077ec03c229');
-
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '0ccb4ed1f69578296a20');
