1d0400e27d1fab4d88c3e1ae3511dd7335e623dd
vanshk141999 <kvansh297@gmail.com>
2023-10-02 15:09:37 +0530

    removed help text default value

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '04728497f7d18aac07fd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '2fb9cce0497a6eb630fc');
