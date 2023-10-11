86e0f74271b18c6d5f4255418041668c7a273913
babu tech <babutech@babus-MacBook-Air.local>
2023-10-11 23:52:12 +0530

    SF-529 multichoice styles switching

--- inc/blocks/multichoice/block.php
+++ inc/blocks/multichoice/block.php
@@ -9,6 +9,7 @@ namespace SureForms\Inc\Blocks\Multichoice;
 
 use SureForms\Inc\Blocks\Base;
 use SureForms\Inc\Sureforms_Helper;
+use SureForms\Inc\Fields\Multichoice_Markup;
 
 /**
  * Multichoice Block.
@@ -23,83 +24,25 @@ class Block extends Base {
 	 * @return string|boolean
 	 */
 	public function render( $attributes, $content = '' ) {
-		$sureforms_helper_instance = new Sureforms_Helper();
-
 		if ( ! empty( $attributes ) ) {
-			$id               = isset( $attributes['id'] ) ? $sureforms_helper_instance->get_string_value( $attributes['id'] ) : '';
-			$required         = isset( $attributes['required'] ) ? $attributes['required'] : false;
-			$single_selection = isset( $attributes['singleSelection'] ) ? $attributes['singleSelection'] : false;
-			$options          = isset( $attributes['options'] ) ? $attributes['options'] : array();
-			$label            = isset( $attributes['label'] ) ? $attributes['label'] : '';
-			$help             = isset( $attributes['help'] ) ? $attributes['help'] : '';
-			$style            = isset( $attributes['style'] ) ? $attributes['style'] : '';
-			$error_msg        = isset( $attributes['errorMsg'] ) ? $attributes['errorMsg'] : '';
-			$classname        = isset( $attributes['className'] ) ? $attributes['className'] : '';
+			$form_id      = isset( $attributes['formId'] ) ? intval( $attributes['formId'] ) : '';
+			$styling      = get_post_meta( Sureforms_Helper::get_integer_value( $form_id ), '_sureforms_form_styling', true ) ? Sureforms_Helper::get_string_value( get_post_meta( Sureforms_Helper::get_integer_value( $form_id ), '_sureforms_form_styling', true ) ) : '';
+			$markup_class = new Multichoice_Markup();
 			ob_start();
-			?>
-		<!-- <div class="sureforms-multi-choice-container main-container frontend-inputs-holder <?php echo esc_attr( $classname ); ?>" id="sureforms-multi-choice-container-<?php echo esc_attr( $id ); ?>">
-			<label class="sf-text-primary"><?php echo esc_html( $label ); ?> 
-				<?php echo $required && $label ? '<span style="color:red;"> *</span>' : ''; ?>
-			</label>
-			<input type="hidden" aria-required="<?php echo esc_attr( $required ? 'true' : 'false' ); ?>" value="<?php echo esc_attr( $single_selection ); ?>" id="sureforms-multi-choice-selection-<?php echo esc_attr( $id ); ?>" />
-			<input type="hidden" value="<?php echo esc_attr( $style ); ?>" id="sureforms-multi-choice-style-<?php echo esc_attr( $id ); ?>" />
-			<input class="sureforms-multi-choice-<?php echo esc_attr( $id ); ?>" aria-required="<?php echo esc_attr( $required ? 'true' : 'false' ); ?>" name="<?php echo esc_attr( $single_selection ? str_replace( ' ', '_', $label . 'SF-divider' . $id ) : str_replace( ' ', '_', $label . 'SF-divider' . $id ) ); ?>" type="hidden" value="">
-				<?php foreach ( $options as $i => $option ) : ?>
-					<div style="display: flex; align-items: center;">
-						<input
-							style="display: <?php echo esc_attr( 'buttons' === $style ? 'none' : 'inherit' ); ?>"
-							class="sureforms-multi-choice"
-							id="sureforms-multi-choice-<?php echo esc_attr( $id . '-' . $i ); ?>"
-							type="<?php echo esc_attr( $single_selection ? 'radio' : 'checkbox' ); ?>"
-							<?php echo esc_attr( $single_selection ? 'name="' . esc_attr( "sf-radio-$id" ) . '"' : '' ); ?>
-						/>
-						<label
-							class="sureforms-multi-choice-label-<?php echo esc_attr( $id ); ?> sureforms-multi-choice-label-button"
-							for="sureforms-multi-choice-<?php echo esc_attr( $id . '-' . $i ); ?>"
-							style="
-							<?php
-							echo esc_attr( 'buttons' === $style ? 'background-color: white; cursor: pointer; border: 2px solid black; border-radius: 10px; padding: .5rem 1rem .5rem 1rem; width: 100%; color: black;' : '' );
-							?>
-							"
-						>
-							<span
-								class="multi-choice-option"
-								id="multi-choice-option-<?php echo esc_attr( $id . '-' . $i ); ?>"
-							>
-								<?php echo esc_html( $option ); ?>
-							</span>
-						</label>
-						<span></span>
-					</div>
-				<?php endforeach; ?>
-				<?php echo '' !== $help ? '<label class="sf-text-secondary sforms-helper-txt">' . esc_html( $help ) . '</label>' : ''; ?>
-				<span style="display:none" class="error-message"><?php echo esc_html( $error_msg ); ?></span>
-		</div> -->
-			<div class="sureforms-multi-choice-container main-container frontend-inputs-holder <?php echo esc_attr( $classname ); ?>" id="sureforms-multi-choice-container-<?php echo esc_attr( $id ); ?>">
-			<input type="hidden" aria-required="<?php echo esc_attr( $required ? 'true' : 'false' ); ?>" value="<?php echo esc_attr( $single_selection ); ?>" id="sureforms-multi-choice-selection-<?php echo esc_attr( $id ); ?>" />
-			<input type="hidden" value="<?php echo esc_attr( $style ); ?>" id="sureforms-multi-choice-style-<?php echo esc_attr( $id ); ?>" />
-			<input class="sureforms-multi-choice-<?php echo esc_attr( $id ); ?>" aria-required="<?php echo esc_attr( $required ? 'true' : 'false' ); ?>" name="<?php echo esc_attr( $single_selection ? str_replace( ' ', '_', $label . 'SF-divider' . $id ) : str_replace( ' ', '_', $label . 'SF-divider' . $id ) ); ?>" type="hidden" value="">
-			<label for="text" class="sf-classic-label-text"><?php echo esc_html( $label ); ?> <?php echo $required && $label ? '<span class="text-red-500"> *</span>' : ''; ?></label>
-				<div class="radio-buttons flex flex-wrap mt-2">
-				<?php foreach ( $options as $i => $option ) : ?>
-					<label class="classic-sf-radio">
-					<input type="<?php echo esc_attr( $single_selection ? 'radio' : 'checkbox' ); ?>" <?php echo esc_attr( $single_selection ? 'name="' . esc_attr( "sf-radio-$id" ) . '"' : '' ); ?> id="sureforms-multi-choice-<?php echo esc_attr( $id . '-' . $i ); ?>" class="sureforms-multi-choice">
-					<div class="flex items-start classic-radio-btn sf-classic-multi-choice">
-						<div class="pr-[5px] relative flex">
-							<i class="fa fa-check-circle text-base" aria-hidden="true"></i>
-							<i class="fa-regular fa-circle text-sm absolute text-gray-300" aria-hidden="true"></i>
-						</div>
-						<div> 
-							<article id="multi-choice-option-<?php echo esc_attr( $id . '-' . $i ); ?>" class="text-sm font-medium leading-6 text-primary_color mt-[-0.5px]"><?php echo esc_html( $option ); ?></article>
-						</div>
-					</div>
-					</label>
-				<?php endforeach; ?>
-				</div>
-				<?php echo '' !== $help ? '<p class="sforms-helper-txt">' . esc_html( $help ) . '</p>' : ''; ?>
-				<p style="display:none" class="error-message"><?php echo esc_html( $error_msg ); ?></p>
-			</div>
-			<?php
+			switch ( $styling ) {
+				case 'inherit':
+					// phpcs:ignore
+					echo $markup_class->default_styling( $attributes );
+					break;
+				case 'classic':
+					// phpcs:ignore
+					echo $markup_class->classic_styling( $attributes );
+					break;
+				default:
+					// phpcs:ignore
+					echo $markup_class->default_styling( $attributes );
+					break;
+			}
 		}
 			return ob_get_clean();
 	}
---
+++ inc/fields/multichoice-markup.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Sureforms Multichoice Markup Class file.
+ *
+ * @package sureforms.
+ * @since 0.0.1
+ */
+
+namespace SureForms\Inc\Fields;
+
+use SureForms\Inc\Traits\Get_Instance;
+use SureForms\Inc\Sureforms_Helper;
+
+/**
+ * SureForms Multichoice Markup Class.
+ *
+ * @since 0.0.1
+ */
+class Multichoice_Markup extends Base {
+	use Get_Instance;
+
+	/**
+	 * Render the sureforms Multichoice default styling block
+	 *
+	 * @param array<mixed> $attributes Block attributes.
+	 *
+	 * @return string|boolean
+	 */
+	public function default_styling( $attributes ) {
+			$id               = isset( $attributes['id'] ) ? Sureforms_Helper::get_string_value( $attributes['id'] ) : '';
+			$required         = isset( $attributes['required'] ) ? $attributes['required'] : false;
+			$single_selection = isset( $attributes['singleSelection'] ) ? $attributes['singleSelection'] : false;
+			$options          = isset( $attributes['options'] ) ? $attributes['options'] : array();
+			$label            = isset( $attributes['label'] ) ? $attributes['label'] : '';
+			$help             = isset( $attributes['help'] ) ? $attributes['help'] : '';
+			$style            = isset( $attributes['style'] ) ? $attributes['style'] : '';
+			$error_msg        = isset( $attributes['errorMsg'] ) ? $attributes['errorMsg'] : '';
+			$classname        = isset( $attributes['className'] ) ? $attributes['className'] : '';
+
+			$output  = '';
+			$output .= '
+		<div class="sureforms-multi-choice-container main-container frontend-inputs-holder ' . esc_attr( $classname ) . '" id="sureforms-multi-choice-container-' . esc_attr( $id ) . '">
+		<label class="sf-text-primary">' . esc_html( $label ) . ' ' . ( $required && $label ? '<span style="color:red;"> *</span>' : '' ) . '</label>
+		<input type="hidden" aria-required="' . esc_attr( $required ? 'true' : 'false' ) . '" value="' . esc_attr( $single_selection ) . '" id="sureforms-multi-choice-selection-' . esc_attr( $id ) . '" />
+		<input type="hidden" value="' . esc_attr( $style ) . '" id="sureforms-multi-choice-style-' . esc_attr( $id ) . '" />
+		<input class="sureforms-multi-choice-' . esc_attr( $id ) . '" aria-required="' . esc_attr( $required ? 'true' : 'false' ) . '" name="' . esc_attr( $single_selection ? str_replace( ' ', '_', $label . 'SF-divider' . $id ) : str_replace( ' ', '_', $label . 'SF-divider' . $id ) ) . '" type="hidden" value="">';
+		if ( is_array( $options ) ) {
+			foreach ( $options as $i => $option ) {
+				$output .= '	<div style="display: flex; align-items: center;">
+					<input
+						style="display: ' . esc_attr( 'buttons' === $style ? 'none' : 'inherit' ) . '"
+						class="sureforms-multi-choice"
+						id="sureforms-multi-choice-' . esc_attr( $id . '-' . $i ) . '"
+						type="' . esc_attr( $single_selection ? 'radio' : 'checkbox' ) . '"
+						' . esc_attr( $single_selection ? 'name="' . esc_attr( "sf-radio-$id" ) . '"' : '' ) . '
+					/>
+					<label
+						class="sureforms-multi-choice-label-' . esc_attr( $id ) . ' sureforms-multi-choice-label-button"
+						for="sureforms-multi-choice-' . esc_attr( $id . '-' . $i ) . '"
+						style="
+						' . esc_attr( 'buttons' === $style ? 'background-color: white; cursor: pointer; border: 2px solid black; border-radius: 10px; padding: .5rem 1rem .5rem 1rem; width: 100%; color: black;' : '' ) . '
+						"
+					>
+						<span
+							class="multi-choice-option"
+							id="multi-choice-option-' . esc_attr( $id . '-' . $i ) . '"
+						>
+							' . esc_html( $option ) . '
+						</span>
+					</label>
+					<span></span>
+				</div>';
+			}
+		}
+
+		$output .= '' . ( '' !== $help ? '<label class="sf-text-secondary sforms-helper-txt">' . esc_html( $help ) . '</label>' : '' ) . '
+			<span style="display:none" class="error-message">' . esc_html( $error_msg ) . '</span>
+	</div>
+		';
+		return $output;
+
+	}
+
+	/**
+	 * Render the sureforms Multichoice classic styling
+	 *
+	 * @param array<mixed> $attributes Block attributes.
+	 *
+	 * @return string|boolean
+	 */
+	public function classic_styling( $attributes ) {
+			$id               = isset( $attributes['id'] ) ? Sureforms_Helper::get_string_value( $attributes['id'] ) : '';
+			$required         = isset( $attributes['required'] ) ? $attributes['required'] : false;
+			$single_selection = isset( $attributes['singleSelection'] ) ? $attributes['singleSelection'] : false;
+			$options          = isset( $attributes['options'] ) ? $attributes['options'] : array();
+			$label            = isset( $attributes['label'] ) ? $attributes['label'] : '';
+			$help             = isset( $attributes['help'] ) ? $attributes['help'] : '';
+			$style            = isset( $attributes['style'] ) ? $attributes['style'] : '';
+			$error_msg        = isset( $attributes['errorMsg'] ) ? $attributes['errorMsg'] : '';
+			$classname        = isset( $attributes['className'] ) ? $attributes['className'] : '';
+			$output           = '';
+			$output          .= '
+			<div class="sureforms-multi-choice-container main-container frontend-inputs-holder ' . esc_attr( $classname ) . '" id="sureforms-multi-choice-container-' . esc_attr( $id ) . '">
+				<input type="hidden" aria-required="' . esc_attr( $required ? 'true' : 'false' ) . '" value="' . esc_attr( $single_selection ) . '" id="sureforms-multi-choice-selection-' . esc_attr( $id ) . '" />
+				<input type="hidden" value="' . esc_attr( $style ) . '" id="sureforms-multi-choice-style-' . esc_attr( $id ) . '" />
+				<input class="sureforms-multi-choice-' . esc_attr( $id ) . '" aria-required="' . esc_attr( $required ? 'true' : 'false' ) . '" name="' . esc_attr( $single_selection ? str_replace( ' ', '_', $label . 'SF-divider' . $id ) : str_replace( ' ', '_', $label . 'SF-divider' . $id ) ) . '" type="hidden" value="">
+				<label for="text" class="sf-classic-label-text">' . esc_html( $label ) . ' ' . ( $required && $label ? '<span class="text-red-500"> *</span>' : '' ) . '</label>
+				<div class="radio-buttons flex flex-wrap mt-2">';
+		if ( is_array( $options ) ) {
+			foreach ( $options as $i => $option ) {
+						$output .= ' <label class="classic-sf-radio">
+						<input type="' . esc_attr( $single_selection ? 'radio' : 'checkbox' ) . '" ' . esc_attr( $single_selection ? 'name="' . esc_attr( "sf-radio-$id" ) . '"' : '' ) . ' id="sureforms-multi-choice-' . esc_attr( $id . '-' . $i ) . '" class="sureforms-multi-choice">
+						<div class="flex items-start classic-radio-btn sf-classic-multi-choice">
+							<div class="pr-[5px] relative flex">
+								<i class="fa fa-check-circle text-base" aria-hidden="true"></i>
+								<i class="fa-regular fa-circle text-sm absolute text-gray-300" aria-hidden="true"></i>
+							</div>
+							<div> 
+								<article id="multi-choice-option-' . esc_attr( $id . '-' . $i ) . '" class="text-sm font-medium leading-6 text-primary_color mt-[-0.5px]">' . esc_html( $option ) . '</article>
+							</div>
+						</div>
+						</label>';
+			}
+		}
+					$output .= '
+					</div>
+					' . ( '' !== $help ? '<p class="sforms-helper-txt" >' . esc_html( $help ) . '</p>' : '' ) . '
+					<p style="display:none" class="error-message">' . esc_html( $error_msg ) . '</p>
+				</div>
+			';
+		return $output;
+
+	}
+
+}
