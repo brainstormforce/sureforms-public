afc0cfb18c8245bfc5e3499ce95149c93dc66841
vanshk141999 <kvansh297@gmail.com>
2023-10-16 17:32:19 +0530

    removed transform attribute

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '87d98bf6115e61b97598');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'de236c0f22dd6f9484d8');
