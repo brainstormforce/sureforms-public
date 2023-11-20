36175bc2f8837fb77c41a8f25e77ea21885e9d9a
vanshk141999 <kvansh297@gmail.com>
2023-11-21 00:42:24 +0530

    change form select dropdown

--- assets/build/formEditor.asset.php
+++ assets/build/formEditor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '69784f9503681af2c4f5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'c027095f7ca2f4639757');
