5205deced14a776bdceb1ef8971202664c413aee
ravinishadcoder <ravicoder264@gmail.com>
2023-12-04 09:54:00 +0530

    added default pop for date picker

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '35a2f12657db4fea617e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'f06c6d6dfdcc556e3c86');
