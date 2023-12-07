812c13e9f7d66a28b6afc5c803e5582a7f49cf36
ravinishadcoder <ravicoder264@gmail.com>
2023-12-07 10:13:34 +0530

    fix lint js-issues

--- assets/build/formEditor.asset.php
+++ assets/build/formEditor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '8b66d49e46eec02ac281');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'edd2ca42d1661e18485c');
---
+++ assets/src/admin/single-form-settings/email-settings/JoditEditor.js
@@ -0,0 +1,37 @@
+import { useState, useRef, useMemo } from '@wordpress/element';
+import JoditEditor from 'jodit-react';
+
+const Editor = ( { placeholder } ) => {
+	const editor = useRef( null );
+	const [ content, setContent ] = useState( '' );
+
+	const config = useMemo(
+		() => ( {
+			readonly: false,
+			placeholder: placeholder || 'Start typings...',
+			useSearch: false,
+			iframe: true,
+			minHeight: 240,
+			spellcheck: true,
+			toolbarButtonSize: 'small',
+			disablePlugins: 'about,fullsize,file,image,image-processor,image-properties,media,powered-by-jodit,speech-recognize,video',
+			buttons: 'bold,italic,underline,strikethrough,eraser,ul,ol,font,fontsize,paragraph,classSpan,lineHeight,superscript,subscript,spellcheck,cut,copy,paste,selectall,source',
+  			toolbarInlineForSelection: true,
+		} ),
+		[ placeholder ]
+	);
+	return (
+		<JoditEditor
+			ref={ editor }
+			value={ content }
+			config={ config }
+			tabIndex={ 0 }
+			onBlur={ ( newContent ) => setContent( newContent ) }
+			onChange={ ( newContent ) => {
+				console.log( { newContent } );
+			} }
+		/>
+	);
+};
+
+export default Editor;
