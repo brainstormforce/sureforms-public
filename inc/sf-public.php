ce6b21c90cd5d087e1796b157205ad22ce6c2d9d
ravinishadcoder <ravicoder264@gmail.com>
2024-01-10 12:25:06 +0530

    change px unit into em

--- inc/sf-public.php
+++ inc/sf-public.php
@@ -70,7 +70,6 @@ class SF_Public {
 		// Int-tel-input CSS.
 		wp_enqueue_style( 'intlTelInput', $css_vendor . 'intl/intlTelInput.min.css', [], SUREFORMS_VER );
 
-		wp_enqueue_script( 'srfm-page-break-script', SUREFORMS_URL . 'assets/build/pageBreakJS.js', [ 'srfm-form-submit' ], SUREFORMS_VER, true );
 		wp_enqueue_script( 'srfm-form-submit', SUREFORMS_URL . 'assets/build/formSubmitJS.js', [], SUREFORMS_VER, true );
 		// Frontend common and validation before submit.
 		wp_enqueue_script( SUREFORMS_SLUG . '-frontend', $js_uri . 'frontend' . $file_prefix . '.js', [], SUREFORMS_VER, true );
@@ -131,6 +130,9 @@ class SF_Public {
 			}
 
 			wp_enqueue_script( SUREFORMS_SLUG . "-{$block_name}", $js_uri . $block_name . $file_prefix . '.js', [], SUREFORMS_VER, true );
+		}
+		if ( 'page-break' === $block_name ) {
+			wp_enqueue_script( 'srfm-page-break-script', SUREFORMS_URL . 'assets/build/pageBreakJS.js', [ 'srfm-form-submit' ], SUREFORMS_VER, true );
 		}
 	}
 
