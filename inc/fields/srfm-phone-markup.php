264ef866be711c2f1bec07cf34a23f8b669b8c3a
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2024-02-26 16:56:59 +0530

    fix : phone unique validation

--- inc/fields/srfm-phone-markup.php
+++ inc/fields/srfm-phone-markup.php
@@ -40,13 +40,13 @@ class SRFM_Phone_Markup extends SRFM_Base {
 		$help         = isset( $attributes['help'] ) ? $attributes['help'] : '';
 		$error_msg    = isset( $attributes['errorMsg'] ) ? $attributes['errorMsg'] : '';
 		$is_unique    = isset( $attributes['isUnique'] ) ? $attributes['isUnique'] : false;
-		$dulicate_msg = isset( $attributes['duplicateMsg'] ) ? $attributes['duplicateMsg'] : '';
+		$duplicate_msg   = isset( $attributes['duplicateMsg'] ) ? $attributes['duplicateMsg'] : '';
 		$classname    = isset( $attributes['className'] ) ? ' ' . $attributes['className'] : '';
 		$auto_country = isset( $attributes['autoCountry'] ) ? $attributes['autoCountry'] : '';
 		$slug         = 'phone';
 
 		$block_width = $field_width ? ' srfm-block-width-' . str_replace( '.', '-', $field_width ) : '';
-
+		$aria_unique          = $is_unique ? 'true' : 'false';
 		$aria_require_attr    = $required ? 'true' : 'false';
 		$placeholder_attr     = $placeholder ? 'placeholder="' . $placeholder . '" ' : '';
 		$input_label_fallback = $label ? $label : __( 'Phone', 'sureforms' );
@@ -58,10 +58,10 @@ class SRFM_Phone_Markup extends SRFM_Base {
 		<div class="srfm-block-single srfm-block srfm-<?php echo esc_attr( $slug ); ?>-block srf-<?php echo esc_attr( $slug ); ?>-<?php echo esc_attr( $block_id ); ?>-block<?php echo esc_attr( $block_width ); ?><?php echo esc_attr( $classname ); ?>">
 				<?php echo wp_kses_post( SRFM_Helper::generate_common_form_markup( $form_id, 'label', $label, $slug, $block_id . $input_label, boolval( $required ) ) ); ?>
 				<div class="srfm-block-wrap">
-					<input type="tel" class="srfm-input-common srfm-input-<?php echo esc_attr( $slug ); ?>" name="<?php echo esc_attr( $unique_slug ); ?>" id="<?php echo esc_attr( $unique_slug ); ?>" aria-required="<?php echo esc_attr( $aria_require_attr ); ?>" auto-country="<?php echo esc_attr( $auto_country ? 'true' : 'false' ); ?>" value="" <?php echo wp_kses_post( $placeholder_attr ); ?>>
+					<input type="tel" class="srfm-input-common srfm-input-<?php echo esc_attr( $slug ); ?>" name="<?php echo esc_attr( $unique_slug ); ?>" id="<?php echo esc_attr( $unique_slug ); ?>" aria-required="<?php echo esc_attr( $aria_require_attr ); ?>" auto-country="<?php echo esc_attr( $auto_country ? 'true' : 'false' ); ?>" value="" data-unique="<?php echo esc_attr( $aria_unique ); ?>" <?php echo wp_kses_post( $placeholder_attr ); ?>>
 				</div>
 				<?php echo wp_kses_post( SRFM_Helper::generate_common_form_markup( $form_id, 'help', '', '', '', false, $help ) ); ?>
-				<?php echo wp_kses_post( SRFM_Helper::generate_common_form_markup( $form_id, 'error', '', '', '', boolval( $required ), '', $error_msg, false, '', true ) ); ?>
+				<?php echo wp_kses_post( SRFM_Helper::generate_common_form_markup( $form_id, 'error', '', '', '', boolval( $required ), '', $error_msg, false, $duplicate_msg, $is_unique ) ); ?>
 			</div>
 		<?php
 		return ob_get_clean();
