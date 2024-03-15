2d26096f6d547977929753550b46a1db21005b9d
Vansh <kvansh297@gmail.com>
2024-03-15 23:47:22 +0530

    latest build

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '6680ab3c551bd390f518');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '244996533eb8e7c2eb5f');
--- assets/build/editor.asset.php
+++ assets/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '335d166e9a18f757c065');
+<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
