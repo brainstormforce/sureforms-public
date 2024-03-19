ccabe6bef1274b69fa640cf1f8e998adeb002f1f
Manoj Rao <62244851+rao-manoj@users.noreply.github.com>
2024-03-19 08:45:46 +0530

    Webhook feature in free version - added upsell message and cta to promoto pro version - added usell message and inline install option for suretriggers

--- src/admin/single-form-settings/components/SingleFormSettingPopup.js
+++ src/admin/single-form-settings/components/SingleFormSettingPopup.js
@@ -5,43 +5,43 @@ import { useState } from '@wordpress/element';
 import svgIcons from '@Image/single-form-logo.json';
 import parse from 'html-react-parser';
 
-const SingleFormSettingsPopup = ( props ) => {
+const SingleFormSettingsPopup = (props) => {
 	const { sureformsKeys, targetTab } = props;
-	const emailIcon = parse( svgIcons.email );
-	const integrationIcon = parse( svgIcons.integration );
+	const emailIcon = parse(svgIcons.email);
+	const integrationIcon = parse(svgIcons.integration);
 	const emailNotificationData = sureformsKeys._srfm_email_notification || [];
-	const [ selectedTab, setSelectedTab ] = useState( targetTab ?? 'email_notification' );
+	const [selectedTab, setSelectedTab] = useState(targetTab ?? 'email_notification');
 	return (
 		<div className="srfm-setting-modal-container">
 			<div className="srfm-modal-sidebar">
 				<div
-					className="srfm-modal-tab"
-					onClick={ () => setSelectedTab( 'email_notification' ) }
+					className={`srfm-modal-tab ${'email_notification' === selectedTab ? 'srfm-modal-tab-active' : ''}`}
+					onClick={() => setSelectedTab('email_notification')}
 				>
-					<span className="srfm-modal-tab-icon">{ emailIcon }</span>
+					<span className="srfm-modal-tab-icon">{emailIcon}</span>
 					<span className="srfm-modal-tab-text">
-						<p>{ __( 'Email Notification', 'sureforms' ) }</p>
+						<p>{__('Email Notification', 'sureforms')}</p>
 					</span>
 				</div>
 				<div
-					className="srfm-modal-tab"
-					onClick={ () => setSelectedTab( 'integration' ) }
+					className={`srfm-modal-tab ${'integration' === selectedTab ? 'srfm-modal-tab-active' : ''}`}
+					onClick={() => setSelectedTab('integration')}
 				>
-					<span className="srfm-modal-tab-icon">{ integrationIcon }</span>
+					<span className="srfm-modal-tab-icon">{integrationIcon}</span>
 					<span className="srfm-modal-tab-text">
-						<p>{ __( 'Integration', 'sureforms' ) }</p>
+						<p>{__('Integration', 'sureforms')}</p>
 					</span>
 				</div>
 			</div>
-			{ /* Modal Content */ }
-			{ 'email_notification' === selectedTab && (
+			{ /* Modal Content */}
+			{'email_notification' === selectedTab && (
 				<EmailNotification
-					emailNotificationData={ emailNotificationData }
+					emailNotificationData={emailNotificationData}
 				/>
-			) }
-			{ 'integration' === selectedTab && (
+			)}
+			{'integration' === selectedTab && (
 				<Integration />
-			) }
+			)}
 		</div>
 	);
 };
