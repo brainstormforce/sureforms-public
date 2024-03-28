b6e5af17a263ba41c0a0a7e389e7e105e42ba321
Vansh <kvansh297@gmail.com>
2024-03-28 14:21:26 +0530

    fixed alias

--- modules/gutenberg/src/blocks/advanced-heading/edit.js
+++ modules/gutenberg/src/blocks/advanced-heading/edit.js
@@ -15,7 +15,7 @@ import DynamicFontLoader from './dynamicFontLoader';
 import { compose } from '@wordpress/compose';
 import AddStaticStyles from '@Controls/AddStaticStyles';
 import AddInitialAttr from '@Controls/addInitialAttr';
-import { useGetCurrentFormId } from '../../../../../src/blocks-attributes/getFormId';
+import { useGetCurrentFormId } from '@SrfmAttributes/getFormId';
 
 const UAGBAdvancedHeading = ( props ) => {
 	const {
