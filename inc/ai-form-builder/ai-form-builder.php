4499b2c2195aac8984aa083601814981ae212474
Vansh <kvansh297@gmail.com>
2024-05-08 11:13:27 +0530

    refactored code and fix checks error

--- inc/ai-form-builder/ai-form-builder.php
+++ inc/ai-form-builder/ai-form-builder.php
@@ -6,9 +6,8 @@
  */
 
 namespace SRFM\Inc\AI_Form_Builder;
+
 use SRFM\Inc\Traits\Get_Instance;
-use SRFM\Inc\Helper;
-use SRFM\Inc\AI_Form_Builder\Field_Mapping;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -23,7 +22,7 @@ class AI_Form_Builder {
 	/**
 	 * The namespace for the Rest Routes.
 	 *
-	 * @since 1.0.0
+	 * @since x.x.x
 	 * @var string
 	 */
 	private $namespace = 'sureforms/v1';
@@ -31,7 +30,7 @@ class AI_Form_Builder {
 	/**
 	 * Instance of this class.
 	 *
-	 * @since 1.0.0
+	 * @since x.x.x
 	 * @var object Class object.
 	 */
 	private static $instance;
@@ -41,52 +40,38 @@ class AI_Form_Builder {
 	/**
 	 * Constructor of this class.
 	 *
-	 * @since 1.0.0
+	 * @since x.x.x
 	 * @return void
 	 */
 	public function __construct() {
-		// Setup the Sidebar Rest Routes.
-		add_action( 'rest_api_init', array( $this, 'register_route' ) );
-
-		// Setup the Sidebar Auth Ajax.
-		// add_action( 'wp_ajax_verify_zip_ai_authenticity', array( $this, 'verify_authenticity' ) );
-
-		// add_action( 'admin_bar_menu', array( $this, 'add_admin_trigger' ), 999 );
-
-		// Render the Sidebar React App in the Footer in the Gutenberg Editor, Admin, and the Front-end.
-		// add_action( 'admin_footer', array( $this, 'render_sidebar_markup' ) );
-		// add_action( 'wp_footer', array( $this, 'render_sidebar_markup' ) );
-
-		// Add the Sidebar to the Gutenberg Editor, Admin, and the Front-end.
-		// add_action( 'admin_enqueue_scripts', array( $this, 'load_sidebar_assets' ) );
-		// add_action( 'wp_enqueue_scripts', array( $this, 'load_sidebar_assets' ) );
+		add_action( 'rest_api_init', [ $this, 'register_route' ] );
 	}
 
 	/**
 	 * Register All Routes.
 	 *
 	 * @hooked - rest_api_init
-	 * @since 1.0.0
+	 * @since x.x.x
 	 * @return void
 	 */
 	public function register_route() {
 		register_rest_route(
 			$this->namespace,
 			'/generate-form',
-			array(
-				array(
+			[
+				[
 					'methods'             => \WP_REST_Server::CREATABLE,
-					'callback'            => array( $this, 'generate_ai_form' ),
+					'callback'            => [ $this, 'generate_ai_form' ],
 					'permission_callback' => function () {
 						return current_user_can( 'edit_posts' );
 					},
-					'args'                => array(
-						'use_system_message' => array(
-							'sanitize_callback' => array( $this, 'sanitize_boolean_field' ),
-						),
-					),
-				),
-			)
+					'args'                => [
+						'use_system_message' => [
+							'sanitize_callback' => [ $this, 'sanitize_boolean_field' ],
+						],
+					],
+				],
+			]
 		);
 	}
 
@@ -94,7 +79,7 @@ class AI_Form_Builder {
 	 * Checks whether the value is boolean or not.
 	 *
 	 * @param mixed $value value to be checked.
-	 * @since 1.0.0
+	 * @since x.x.x
 	 * @return boolean
 	 */
 	public function sanitize_boolean_field( $value ) {
@@ -105,7 +90,7 @@ class AI_Form_Builder {
 	 * Fetches ai data from the middleware server - this will be merged with the get_credit_server_response() function.
 	 *
 	 * @param \WP_REST_Request $request request object.
-	 * @since 1.0.0
+	 * @since x.x.x
 	 * @return void
 	 */
 	public function generate_ai_form( $request ) {
@@ -113,14 +98,14 @@ class AI_Form_Builder {
 		// Get the params.
 		$params = $request->get_params();
 
-		// If the nessage array doesn't exist, abandon ship.
+		// If the message array doesn't exist, abandon ship.
 		if ( empty( $params['message_array'] ) || ! is_array( $params['message_array'] ) ) {
-			wp_send_json_error( array( 'message' => __( 'The message array was not supplied', 'zip-ai' ) ) );
+			wp_send_json_error( [ 'message' => __( 'The message array was not supplied', 'sureforms' ) ] );
 		}
 
 		// Set the token count to 0, and create messages array.
 		$token_count = 0;
-		$messages    = array();
+		$messages    = [];
 
 		// Start with the last message - going upwards until the token count hits 2000.
 		foreach ( array_reverse( $params['message_array'] ) as $current_message ) {
@@ -129,43 +114,17 @@ class AI_Form_Builder {
 				continue;
 			}
 
-			// Get the token count, and if it's greater than 2000, break out of the loop.
-			// $token_count += Helper::get_token_count( $current_message['content'] );
-			// if ( $token_count >= 2000 ) {
-			// 	break;
-			// }
-
 			// Add the message to the start of the messages to send to the SCS Middleware.
 			array_unshift( $messages, $current_message );
 		}
 
 		// Finally add the system message to the start of the array.
-        if (!empty($params['use_system_message'])) {
-            array_unshift(
-                $messages,
-                array(
-                    'role'    => 'system',
-                    'content' => 'You are expert form designer. I will tell you a form description and you have to provide form structure in json format only. Choose the standard HTML form fiields Be specific. Do not mention any description. Respond in the valid JSON format: { "row1": { "0": { "first_name": { "type": "text", "width": 50, "label": "First Name", "placeholder": "Enter your first name", "required": true } }, "1": { "last_name": { "type": "text", "width": 50, "label": "Last Name", "placeholder": "Enter your last name", "required": true } } }, "row2": { "0": { "email": { "type": "email", "width": 100, "label": "Email", "placeholder": "Enter your email", "required": true } } } } and so on... If any error return an empty json.',
-                )
-            );
-        }
-        
-
-		// Out custom endpoint to get OpenAi data.
-		// $endpoint = ZIP_AI_CREDIT_SERVER_API . 'chat/completions';
-        $endpoint = 'https://api.openai.com/v1/chat/completions';
-		$data     = array(
-			// 'temperature'       => 0.7,
-			// 'top_p'             => 1,
-			// 'frequency_penalty' => 0.8,
-			// 'presence_penalty'  => 1,
-			'model'             => 'gpt-3.5-turbo',
-            // 'max_tokens'        => 2048,
-			'messages'          => [
-                array(
-                    'role'    => 'system',
-                    // 'content' => 'You are an expert form designer. Your expertise is needed to craft a JSON structure that accurately represents a form based on the following description. Your JSON structure should utilize standard HTML form fields and adhere to specific requirements. It\'s essential to ensure the JSON structure you provide is valid and easily interpretable for implementation.',
-					'content' => 'Based on the description, generate a survey with questions array where every element has five fields: label, fieldType, placeholder, required, helpText. fieldType can be of these options input, email, url, textarea, checkbox, gdpr, number, phone, dropdown, address, address-compact, hidden, rating, upload, date-time, number-slider, page-break, multi-choice.
+		if ( ! empty( $params['use_system_message'] ) ) {
+			array_unshift(
+				$messages,
+				[
+					'role'    => 'system',
+					'content' => 'Based on the description, generate a survey with questions array where every element has five fields: label, fieldType, placeholder, required, helpText. fieldType can only be of these options input, email, url, textarea, checkbox, gdpr, number, phone, dropdown, address, address-compact, hidden, rating, upload, date-time, number-slider, page-break, multi-choice. fieldType can be used in any order and any number of times.
 					
 					Here are the field specific properties that you need to return with that specific field:
 					
@@ -177,43 +136,42 @@ class AI_Form_Builder {
 
 					4) consider multi step form as page-break and you can use it multiple time if required and use it only if asked to.
 					
-					It is essential to provide a valid JSON structure. If there is an error, return an empty JSON. Make sure you don\'t entertain the empty cases, like Hey there, or kind of message, just check for the proper prompt and understand if the prompt has the word form or any kind of direction to create the form',
-                ),
-                array(
-                    'role'    => 'user',
-                    'content' => $params['message_array'][0]['content'],
-                ),
-            ],
-		);
+					It is essential to provide a valid JSON structure. If there is an error, return an empty JSON. Make sure you don\'t entertain the empty cases, like Hey there, or kind of message, just check for the proper prompt and understand if the prompt has the word form or any kind of direction to create the form.',
+				]
+			);
+		}
+
+		// send the request to the open ai server.
+		$endpoint = 'https://api.openai.com/v1/chat/completions';
+		$data     = [
+			'model'    => 'gpt-3.5-turbo',
+			'messages' => $messages,
+		];
 
 		$response = wp_remote_post(
 			$endpoint,
-			array(
-				'headers' => array(
-					'Authorization' => 'Bearer ' . 'sk-proj-LgSXu9dbBmhYYjYNRdDkT3BlbkFJlvtzD9BxqtrXze3ya9cg',
-                    'Content-Type'  => 'application/json',
-				),
-                'body'    => json_encode( $data ),
-                // 'body'    => $data,
-                'timeout' => 30, // phpcs:ignore WordPressVIPMinimum.Performance.RemoteRequestTimeout.timeout_timeout -- 30 seconds is required sometime for open ai responses
-			)
+			[
+				'body'    => wp_json_encode( $data ) ? wp_json_encode( $data ) : '',
+				'timeout' => 30, // phpcs:ignore WordPressVIPMinimum.Performance.RemoteRequestTimeout.timeout_timeout -- 30 seconds is required sometime for open ai responses
+				'headers' => [
+					'Authorization' => 'Bearer sk-proj-LgSXu9dbBmhYYjYNRdDkT3BlbkFJlvtzD9BxqtrXze3ya9cg',
+					'Content-Type'  => 'application/json',
+				],
+			]
 		);
 
 		if ( is_wp_error( $response ) ) {
-			wp_send_json_error( array( 'message' => __( 'Something went wrong', 'zip-ai' ) ) );
+			wp_send_json_error( [ 'message' => __( 'Something went wrong', 'sureforms' ) ] );
 		} else {
 			$response_body = json_decode( wp_remote_retrieve_body( $response ), true );
 
 			if ( is_array( $response_body ) && is_array( $response_body['choices'] ) && ! empty( $response_body['choices'][0]['message']['content'] ) ) {
 
-                // create post content by mapping the json data to the form fields. Pass the data as array.
-				// $post_content = Field_Mapping::generate_gutenberg_fields_from_json($response_body['choices'][0]['message']['content']);
-
+				// Increment the request count.
 				$request_count = get_option( 'srfm_ai_request_count', 0 );
-
-				// Update the request count.
 				update_option( 'srfm_ai_request_count', $request_count + 1 );
 
+				// send the response back to the client.
 				wp_send_json_success( wp_remote_retrieve_body( $response ) );
 			} elseif ( is_array( $response_body ) && ! empty( $response_body['error'] ) ) {
 				$message = '';
@@ -226,26 +184,26 @@ class AI_Form_Builder {
 					$message = ! empty( $message ) ? $message : $response_body['error'];
 				}
 
-				wp_send_json_error( array( 'message' => $message ) );
+				wp_send_json_error( [ 'message' => $message ] );
 			} else {
-				wp_send_json_error( array( 'message' => __( 'Something went wrong', 'zip-ai' ) ) );
+				wp_send_json_error( [ 'message' => __( 'Something went wrong', 'sureforms' ) ] );
 			}
 		}//end if
 	}
 
 	/**
-	 * This function converts the code recieved from scs to a readable error message.
+	 * This function converts the code received from scs to a readable error message.
 	 * Useful to provide better language for error codes.
 	 *
 	 * @param string $code error code received from SCS ( Credits server ).
-	 * @since 1.0.0
+	 * @since x.x.x
 	 * @return string
 	 */
 	private function custom_message( $code ) {
-		$message_array = array(
-			'no_auth'              => __( 'Invalid auth token.', 'zip-ai' ),
-			'insufficient_credits' => __( 'You have no credits left.', 'zip-ai' ),
-		);
+		$message_array = [
+			'no_auth'              => __( 'Invalid auth token.', 'sureforms' ),
+			'insufficient_credits' => __( 'You have no credits left.', 'sureforms' ),
+		];
 
 		return isset( $message_array[ $code ] ) ? $message_array[ $code ] : '';
 	}
--- inc/ai-form-builder/field-mapping.php
+++ inc/ai-form-builder/field-mapping.php
@@ -18,197 +18,177 @@ if ( ! defined( 'ABSPATH' ) ) {
  * SureForms AI Form Builder Class.
  */
 class Field_Mapping {
-    use Get_Instance;
+	use Get_Instance;
 
-    /**
-     * The namespace for the Rest Routes.
-     *  
-     * @since 1.0.0
-     * @var string
-     */
-    private $namespace = 'sureforms/v1';
+	/**
+	 * The namespace for the Rest Routes.
+	 *
+	 * @since x.x.x
+	 * @var string
+	 */
+	private $namespace = 'sureforms/v1';
 
-    public function __construct() {
-		// Setup the Sidebar Rest Routes.
-		add_action( 'rest_api_init', array( $this, 'register_route' ) );
-
-		// Setup the Sidebar Auth Ajax.
-		// add_action( 'wp_ajax_verify_zip_ai_authenticity', array( $this, 'verify_authenticity' ) );
-
-		// add_action( 'admin_bar_menu', array( $this, 'add_admin_trigger' ), 999 );
-
-		// Render the Sidebar React App in the Footer in the Gutenberg Editor, Admin, and the Front-end.
-		// add_action( 'admin_footer', array( $this, 'render_sidebar_markup' ) );
-		// add_action( 'wp_footer', array( $this, 'render_sidebar_markup' ) );
-
-		// Add the Sidebar to the Gutenberg Editor, Admin, and the Front-end.
-		// add_action( 'admin_enqueue_scripts', array( $this, 'load_sidebar_assets' ) );
-		// add_action( 'wp_enqueue_scripts', array( $this, 'load_sidebar_assets' ) );
+	/**
+	 * Constructor of this class.
+	 *
+	 * @since x.x.x
+	 * @return void
+	 */
+	public function __construct() {
+		add_action( 'rest_api_init', [ $this, 'register_route' ] );
 	}
 
 	/**
 	 * Register All Routes.
 	 *
 	 * @hooked - rest_api_init
-	 * @since 1.0.0
+	 * @since x.x.x
 	 * @return void
 	 */
 	public function register_route() {
 		register_rest_route(
 			$this->namespace,
 			'/map-fields',
-			array(
-				array(
+			[
+				[
 					'methods'             => \WP_REST_Server::CREATABLE,
-					'callback'            => array( $this, 'generate_gutenberg_fields_from_questions' ),
+					'callback'            => [ $this, 'generate_gutenberg_fields_from_questions' ],
 					'permission_callback' => function () {
 						return current_user_can( 'edit_posts' );
-					}
-				),
-			)
+					},
+				],
+			]
 		);
 	}
-    public static function generate_gutenberg_fields_from_questions($request) {
 
-        // Get questions from request
-        $params = $request->get_params();
+	/**
+	 * Generate Gutenberg Fields from AI data.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 * @return string
+	 */
+	public static function generate_gutenberg_fields_from_questions( $request ) {
 
-        // get form_data from request
+		// Get params from request.
+		$params = $request->get_params();
 
-        $questions = $params['form_data']['questions'];
+		// Get questions from form data.
+		$questions = $params['form_data']['questions'];
 
-        if ( $questions === null ){
-            $questions = $params['form_data'];
-        }
+		// Check if questions are null then set it to form_data.
+		if ( null === $questions ) {
+			$questions = $params['form_data'];
+		}
 
-        
+		// Initialize post content string.
+		$post_content = '';
 
-        // Initialize post content string
-        $post_content = '';
-    
-        // Loop through questions
-        foreach ($questions as $index => $question) {
-            // Initialize common attributes
-            $common_attributes = [
-                'block_id' => bin2hex(random_bytes(4)), // Generate random block_id
-                // 'required' => true, // Default required attribute
-                'formId' => 0 // Set your formId here
-            ];
-    
-            // Merge common attributes with question attributes
-            $merged_attributes = array_merge($common_attributes, [
-                'label' => $question['label'],
-                'required' => $question['required'],
-                'placeholder' => $question['placeholder'],
-                'help' => $question['helpText'],
-                'fieldWidth' => $question['fieldWidth']
-            ]);
+		// Loop through questions.
+		foreach ( $questions as $index => $question ) {
+			// Initialize common attributes.
+			$common_attributes = [
+				'block_id' => bin2hex( random_bytes( 4 ) ), // Generate random block_id.
+				'formId'   => 0, // Set your formId here.
+			];
 
-            // exclude label in page-break
+			// Merge common attributes with question attributes.
+			$merged_attributes = array_merge(
+				$common_attributes,
+				[
+					'label'       => $question['label'],
+					'required'    => $question['required'],
+					'placeholder' => $question['placeholder'],
+					'help'        => $question['helpText'],
+					'fieldWidth'  => $question['fieldWidth'],
+				]
+			);
 
-            // if ($question['fieldType'] === 'page-break') {
-            //     unset($merged_attributes['label']);
-            // }
-    
-            // Determine field type based on fieldType
-            switch ($question['fieldType']) {
-                case 'input':
-                    $post_content .= '<!-- wp:srfm/input ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                case 'email':
-                    $post_content .= '<!-- wp:srfm/email ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                case 'number':
-                    $post_content .= '<!-- wp:srfm/number ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                case 'textarea':
-                    $post_content .= '<!-- wp:srfm/textarea ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                case 'dropdown':
-                    // Check if fieldOptions are provided
-                    if (isset($question['fieldOptions']) && is_array($question['fieldOptions'])) {
-                        $merged_attributes['options'] = $question['fieldOptions'];
-                    } else {
-                        // Default options
-                        $merged_attributes['options'] = ['Option 1', 'Option 2', 'Option 3'];
-                    }
-                    $post_content .= '<!-- wp:srfm/dropdown ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                case 'checkbox':
-                    $post_content .= '<!-- wp:srfm/checkbox ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                case 'date-time':
-                    $post_content .= '<!-- wp:srfm/date-time-picker ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                case 'upload':
-                    $post_content .= '<!-- wp:srfm/upload ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // case for address
-                case 'address':
-                    $post_content .= '<!-- wp:srfm/address ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // case for address-compact
-                case 'address-compact':
-                    $post_content .= '<!-- wp:srfm/address-compact ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // case for inline-button
-                case 'inline-button':
-                    $post_content .= '<!-- wp:srfm/inline-button ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // case for hidden
-                case 'hidden':
-                    $post_content .= '<!-- wp:srfm/hidden ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // case for rating
-                case 'rating':
-                    if (isset($question['fieldOptions']) && is_array($question['fieldOptions'])) {
-                        $merged_attributes['ratingBoxHelpText'] = $question['helpText'];
-                    }
-                    $post_content .= '<!-- wp:srfm/rating ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // case for phone
-                case 'phone':
-                    $post_content .= '<!-- wp:srfm/phone ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // case for gdpr
-                case 'gdpr':
-                    $post_content .= '<!-- wp:srfm/gdpr ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // case for number-slider
-                case 'number-slider':
-                    $post_content .= '<!-- wp:srfm/number-slider ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // case for page-break
-                case 'page-break':
-                    $post_content .= '<!-- wp:srfm/page-break ' . json_encode($common_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // multi choice
-                case 'multi-choice':
-                    // Check if fieldOptions are provided
-                    if (isset($question['fieldOptions']) && is_array($question['fieldOptions'])) {
-                        $merged_attributes['options'] = $question['fieldOptions'];
-                        $merged_attributes['singleSelection'] = $question['singleSelection'];
-                    } else {
-                        // Default options
-                        $merged_attributes['options'] = array(
-                            array("optionTitle" => "Option 1"),
-                            array("optionTitle" => "Option 2"),
-                            array("optionTitle" => "Option 3"),
-                            array("optionTitle" => "Option 4")
-                        );
-                    }
-                    $post_content .= '<!-- wp:srfm/multi-choice ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-                    break;
-                    // case for multi-choice
-                default:
-                    // Unsupported field type
-                    $post_content .= '<!-- wp:srfm/' . $question['fieldType'] . ' ' . json_encode($merged_attributes) . ' /-->' . PHP_EOL;
-            }
-        }
-    
-        return $post_content;
-    }
-    
-    
-        
+			// Determine field type based on fieldType.
+			switch ( $question['fieldType'] ) {
+				case 'input':
+					$post_content .= '<!-- wp:srfm/input ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'email':
+					$post_content .= '<!-- wp:srfm/email ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'number':
+					$post_content .= '<!-- wp:srfm/number ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'textarea':
+					$post_content .= '<!-- wp:srfm/textarea ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'dropdown':
+					// Check if fieldOptions are provided.
+					if ( isset( $question['fieldOptions'] ) && is_array( $question['fieldOptions'] ) ) {
+						$merged_attributes['options'] = $question['fieldOptions'];
+					} else {
+						// Default options.
+						$merged_attributes['options'] = [ 'Option 1', 'Option 2', 'Option 3' ];
+					}
+					$post_content .= '<!-- wp:srfm/dropdown ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'checkbox':
+					$post_content .= '<!-- wp:srfm/checkbox ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'date-time':
+					$post_content .= '<!-- wp:srfm/date-time-picker ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'upload':
+					$post_content .= '<!-- wp:srfm/upload ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'address':
+					$post_content .= '<!-- wp:srfm/address ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'address-compact':
+					$post_content .= '<!-- wp:srfm/address-compact ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'inline-button':
+					$post_content .= '<!-- wp:srfm/inline-button ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'hidden':
+					$post_content .= '<!-- wp:srfm/hidden ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'rating':
+					if ( isset( $question['fieldOptions'] ) && is_array( $question['fieldOptions'] ) ) {
+						$merged_attributes['ratingBoxHelpText'] = $question['helpText'];
+					}
+					$post_content .= '<!-- wp:srfm/rating ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'phone':
+					$post_content .= '<!-- wp:srfm/phone ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'gdpr':
+					$post_content .= '<!-- wp:srfm/gdpr ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'number-slider':
+					$post_content .= '<!-- wp:srfm/number-slider ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'page-break':
+					$post_content .= '<!-- wp:srfm/page-break ' . wp_json_encode( $common_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				case 'multi-choice':
+					// Check if fieldOptions are provided.
+					if ( isset( $question['fieldOptions'] ) && is_array( $question['fieldOptions'] ) ) {
+						$merged_attributes['options']         = $question['fieldOptions'];
+						$merged_attributes['singleSelection'] = $question['singleSelection'];
+					} else {
+						// Default options.
+						$merged_attributes['options'] = [
+							[ 'optionTitle' => 'Option 1' ],
+							[ 'optionTitle' => 'Option 2' ],
+							[ 'optionTitle' => 'Option 3' ],
+							[ 'optionTitle' => 'Option 4' ],
+						];
+					}
+					$post_content .= '<!-- wp:srfm/multi-choice ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+					break;
+				default:
+					// Unsupported field type - fallback to input.
+					$post_content .= '<!-- wp:srfm/' . $question['fieldType'] . ' ' . wp_json_encode( $merged_attributes ) . ' /-->' . PHP_EOL;
+			}
+		}
+
+		return $post_content;
+	}
+
 }
