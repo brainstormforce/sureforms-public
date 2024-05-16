e14bc96cab2b88671f002e6aaed33fafac4c0f23
Vansh <kvansh297@gmail.com>
2024-05-16 19:32:06 +0530

    lint js fix

--- src/admin/components/template-picker/components/Header.js
+++ src/admin/components/template-picker/components/Header.js
@@ -53,52 +53,52 @@ const Header = () => {
 			{ srfm_admin.is_authorized &&
 			page === 'add-new-form' &&
 			method === 'ai' ? (
-				<div className="srfm-tp-header-credits-ctn">
-					<div
-						style={ {
+					<div className="srfm-tp-header-credits-ctn">
+						<div
+							style={ {
 							// if popover is open, change background color
-							background: showRevokePopover ? '#F3F4F6' : 'white',
-						} }
-						className="srfm-tp-header-credits"
-						onClick={ () => {
-							setShowRevokePopover( ! showRevokePopover );
-						} }
-					>
-						<span className="srfm-tp-header-credits-left">
-							{ formatNumber( creditsLeft ) }
-						</span>
-						<BsLightningCharge />
+								background: showRevokePopover ? '#F3F4F6' : 'white',
+							} }
+							className="srfm-tp-header-credits"
+							onClick={ () => {
+								setShowRevokePopover( ! showRevokePopover );
+							} }
+						>
+							<span className="srfm-tp-header-credits-left">
+								{ formatNumber( creditsLeft ) }
+							</span>
+							<BsLightningCharge />
+						</div>
+						{ showRevokePopover && (
+							<CreditDetailsPopup
+								setShowRevokePopover={ setShowRevokePopover }
+								setShowRevokeConfirmation={
+									setShowRevokeConfirmation
+								}
+								creditsLeft={ creditsLeft }
+							/>
+						) }
+						<div
+							className="srfm-tp-header-close"
+							onClick={ () => {
+								window.location.href =
+								'/wp-admin/admin.php?page=sureforms_menu';
+							} }
+						>
+							<div>{ ICONS.close }</div>
+						</div>
 					</div>
-					{ showRevokePopover && (
-						<CreditDetailsPopup
-							setShowRevokePopover={ setShowRevokePopover }
-							setShowRevokeConfirmation={
-								setShowRevokeConfirmation
-							}
-							creditsLeft={ creditsLeft }
-						/>
-					) }
+				) : (
 					<div
 						className="srfm-tp-header-close"
 						onClick={ () => {
 							window.location.href =
-								'/wp-admin/admin.php?page=sureforms_menu';
+							'/wp-admin/admin.php?page=sureforms_menu';
 						} }
 					>
 						<div>{ ICONS.close }</div>
 					</div>
-				</div>
-			) : (
-				<div
-					className="srfm-tp-header-close"
-					onClick={ () => {
-						window.location.href =
-							'/wp-admin/admin.php?page=sureforms_menu';
-					} }
-				>
-					<div>{ ICONS.close }</div>
-				</div>
-			) }
+				) }
 		</div>
 	);
 };
