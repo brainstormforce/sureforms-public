5e6a9f9a2b340d0afadb0538dc2c2a122ce45d2d
osk02 <omk@bsf.io>
2024-06-10 18:08:31 +0530

    dropdown accessibility fix

--- assets/js/unminified/blocks/dropdown.js
+++ assets/js/unminified/blocks/dropdown.js
@@ -1,11 +1,22 @@
+const { speak } = wp.a11y;
+
 function initializeDropdown() {
 	const dropDownSelector = document.querySelectorAll(
 		'.srfm-dropdown-common'
 	);
 
 	dropDownSelector.forEach( ( element ) => {
+		const placeholderOption = element.querySelector( '.srfm-dropdown-placeholder' );
+		const placeholderText = placeholderOption ? placeholderOption.innerText : '';
 		if ( element ) {
-			const config = {};
+			const config = {
+				onDropdownOpen( dropdown ) {
+					speak( placeholderText );
+				},
+				onChange( value ) {
+					if ( value ) speak( value );
+				}
+			};
 			new TomSelect( element, config );
 		}
 	} );
