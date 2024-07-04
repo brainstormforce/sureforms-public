56c5734266ada2935ed88d1d0096082db374bad1
osk02 <omk@bsf.io>
2024-07-04 16:23:41 +0530

    added anchor tag for buttons

--- src/admin/components/UpgradeToProButton.js
+++ src/admin/components/UpgradeToProButton.js
@@ -1,14 +1,12 @@
 import { __ } from '@wordpress/i18n';
 
-const UpgradeToProButton = ( { className } ) => {
+const UpgradeToProButton = ( props ) => {
 	return (
-		<button onClick={
-			() => {
-				window.open( 'https://sureforms.com/pricing' );
-			}
-		} className={ className }>
-			{ __( 'Upgrade to Pro', 'sureforms' ) }
-		</button>
+		<a href="https://sureforms.com/pricing" target="_blank" rel="noreferrer">
+			<button { ...props }>
+				{ __( 'Upgrade to Pro', 'sureforms' ) }
+			</button>
+		</a>
 	);
 };
 
