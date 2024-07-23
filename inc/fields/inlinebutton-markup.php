86789b0280a14e3cd5c85cdcbe3f7d0663d6c6c7
Aditya Jain <adi3890@gmail.com>
2024-07-23 17:46:19 +0530

    adding styles in frontend

--- inc/fields/inlinebutton-markup.php
+++ inc/fields/inlinebutton-markup.php
@@ -115,48 +115,48 @@ class Inlinebutton_Markup extends Base {
 	 * @param array<mixed> $attributes Block attributes.
 	 * @since 0.0.2
 	 */
-	public function __construct( $attributes ) {
-		$this->set_properties( $attributes );
-		$this->slug                    = 'inline-button';
-		$this->button_text             = isset( $attributes['buttonText'] ) ? $attributes['buttonText'] : '';
-		$this->btn_from_theme          = Helper::get_meta_value( $this->form_id, '_srfm_inherit_theme_button' );
-		$this->is_page_break           = defined( 'SRFM_PRO_VER' ) && Helper::get_meta_value( $this->form_id, '_srfm_is_page_break' );
-		$this->captcha_security_type   = Helper::get_meta_value( $this->form_id, '_srfm_captcha_security_type' );
-		$this->recaptcha_version       = Helper::get_meta_value( $this->form_id, '_srfm_form_recaptcha' );
-		$this->google_captcha_site_key = '';
-		$this->global_setting_options  = [];
-		if ( 'none' !== $this->captcha_security_type ) {
-			$this->global_setting_options = get_option( 'srfm_security_settings_options' );
-		}
+public function __construct( $attributes ) {
+	$this->set_properties( $attributes );
+	$this->slug                    = 'inline-button';
+	$this->button_text             = isset( $attributes['buttonText'] ) ? $attributes['buttonText'] : '';
+	$this->btn_from_theme          = Helper::get_meta_value( $this->form_id, '_srfm_inherit_theme_button' );
+	$this->is_page_break           = defined( 'SRFM_PRO_VER' ) && Helper::get_meta_value( $this->form_id, '_srfm_is_page_break' );
+	$this->captcha_security_type   = Helper::get_meta_value( $this->form_id, '_srfm_captcha_security_type' );
+	$this->recaptcha_version       = Helper::get_meta_value( $this->form_id, '_srfm_form_recaptcha' );
+	$this->google_captcha_site_key = '';
+	$this->global_setting_options  = [];
+	if ( 'none' !== $this->captcha_security_type ) {
+		$this->global_setting_options = get_option( 'srfm_security_settings_options' );
+	}
 
-		if ( is_array( $this->global_setting_options ) ) {
-			switch ( $this->recaptcha_version ) {
-				case 'v2-checkbox':
-					$this->google_captcha_site_key = isset( $this->global_setting_options['srfm_v2_checkbox_site_key'] ) ? $this->global_setting_options['srfm_v2_checkbox_site_key'] : '';
-					break;
-				case 'v2-invisible':
-					$this->google_captcha_site_key = isset( $this->global_setting_options['srfm_v2_invisible_site_key'] ) ? $this->global_setting_options['srfm_v2_invisible_site_key'] : '';
-					break;
-				case 'v3-reCAPTCHA':
-					$this->google_captcha_site_key = isset( $this->global_setting_options['srfm_v3_site_key'] ) ? $this->global_setting_options['srfm_v3_site_key'] : '';
-					break;
-				default:
-					break;
-			}
-			if ( 'cf-turnstile' === $this->captcha_security_type ) {
-				$this->cf_turnstile_site_key = isset( $this->global_setting_options['srfm_cf_turnstile_site_key'] ) ? $this->global_setting_options['srfm_cf_turnstile_site_key'] : '';
-				$this->cf_appearance_mode    = isset( $this->global_setting_options['srfm_cf_appearance_mode'] ) ? $this->global_setting_options['srfm_cf_appearance_mode'] : 'auto';
-			}
-			if ( 'hcaptcha' === $this->captcha_security_type ) {
-				$this->hcaptcha_site_key = isset( $this->global_setting_options['srfm_hcaptcha_site_key'] ) ? $this->global_setting_options['srfm_hcaptcha_site_key'] : '';
-			}
+	if ( is_array( $this->global_setting_options ) ) {
+		switch ( $this->recaptcha_version ) {
+			case 'v2-checkbox':
+				$this->google_captcha_site_key = isset( $this->global_setting_options['srfm_v2_checkbox_site_key'] ) ? $this->global_setting_options['srfm_v2_checkbox_site_key'] : '';
+				break;
+			case 'v2-invisible':
+				$this->google_captcha_site_key = isset( $this->global_setting_options['srfm_v2_invisible_site_key'] ) ? $this->global_setting_options['srfm_v2_invisible_site_key'] : '';
+				break;
+			case 'v3-reCAPTCHA':
+				$this->google_captcha_site_key = isset( $this->global_setting_options['srfm_v3_site_key'] ) ? $this->global_setting_options['srfm_v3_site_key'] : '';
+				break;
+			default:
+				break;
 		}
-		$theme_name               = wp_get_theme()->get( 'Name' );
-		$this->add_button_padding = true;
-		if ( 'Astra' === $theme_name || 'Blocksy' === $theme_name ) {
-			$this->add_button_padding = false;
+		if ( 'cf-turnstile' === $this->captcha_security_type ) {
+			$this->cf_turnstile_site_key = isset( $this->global_setting_options['srfm_cf_turnstile_site_key'] ) ? $this->global_setting_options['srfm_cf_turnstile_site_key'] : '';
+			$this->cf_appearance_mode    = isset( $this->global_setting_options['srfm_cf_appearance_mode'] ) ? $this->global_setting_options['srfm_cf_appearance_mode'] : 'auto';
+		}
+		if ( 'hcaptcha' === $this->captcha_security_type ) {
+			$this->hcaptcha_site_key = isset( $this->global_setting_options['srfm_hcaptcha_site_key'] ) ? $this->global_setting_options['srfm_hcaptcha_site_key'] : '';
 		}
 	}
+	$theme_name               = wp_get_theme()->get( 'Name' );
+	$this->add_button_padding = true;
+	if ( 'Astra' === $theme_name || 'Blocksy' === $theme_name ) {
+		$this->add_button_padding = false;
+	}
+}
 
 	/**
 	 * Render inline button markup
@@ -164,8 +164,8 @@ class Inlinebutton_Markup extends Base {
 	 * @since 0.0.2
 	 * @return string|boolean|void
 	 */
-	public function markup() {
-		ob_start(); ?>
+public function markup() {
+	ob_start(); ?>
 		<?php if ( ! $this->is_page_break ) : ?>
 			<?php if ( $this->captcha_security_type && 'none' !== $this->captcha_security_type ) : ?>
 			<div class="srfm-captcha-container">
@@ -181,39 +181,39 @@ class Inlinebutton_Markup extends Base {
 			<div class="srfm-validation-error" id="captcha-error" style="display: none;"><?php echo esc_attr__( 'Please verify that you are not a robot.', 'sureforms' ); ?></div>
 			</div>
 			<div data-block-id="<?php echo esc_attr( $this->block_id ); ?>" class="<?php echo esc_attr( $this->class_name ); ?> <?php echo esc_attr( $this->conditional_class ); ?> srf-<?php echo esc_attr( $this->slug ); ?>-<?php echo esc_attr( $this->block_id ); ?>-block<?php echo esc_attr( $this->block_width ); ?> srfm-block srfm-custom-button-ctn <?php echo esc_attr( '1' === $this->btn_from_theme ? 'wp-block-button' : '' ); ?>">
-			<?php echo wp_kses_post( Helper::generate_common_form_markup( $this->form_id, 'label', '‎', '', '', false ) ); ?>
-			<?php
-			if ( 'g-recaptcha' === $this->captcha_security_type ) :
-				if ( 'v3-reCAPTCHA' === $this->recaptcha_version ) :
-					wp_enqueue_script( 'srfm-google-recaptchaV3', 'https://www.google.com/recaptcha/api.js?render=' . esc_js( $this->google_captcha_site_key ), [], SRFM_VER, true );
-				endif;
+				<?php echo wp_kses_post( Helper::generate_common_form_markup( $this->form_id, 'label', '‎', '', '', false ) ); ?>
+				<?php
+				if ( 'g-recaptcha' === $this->captcha_security_type ) :
+					if ( 'v3-reCAPTCHA' === $this->recaptcha_version ) :
+						wp_enqueue_script( 'srfm-google-recaptchaV3', 'https://www.google.com/recaptcha/api.js?render=' . esc_js( $this->google_captcha_site_key ), [], SRFM_VER, true );
+					endif;
 
-				if ( 'v2-checkbox' === $this->recaptcha_version ) :
-					wp_enqueue_script( 'google-recaptcha', 'https://www.google.com/recaptcha/api.js', [], SRFM_VER, true );
-				endif;
+					if ( 'v2-checkbox' === $this->recaptcha_version ) :
+						wp_enqueue_script( 'google-recaptcha', 'https://www.google.com/recaptcha/api.js', [], SRFM_VER, true );
+					endif;
 
-				if ( 'v2-invisible' === $this->recaptcha_version ) :
-					wp_enqueue_script( 'google-recaptcha-invisible', 'https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit', [ SRFM_SLUG . '-form-submit' ], SRFM_VER, true );
-				endif;
+					if ( 'v2-invisible' === $this->recaptcha_version ) :
+						wp_enqueue_script( 'google-recaptcha-invisible', 'https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit', [ SRFM_SLUG . '-form-submit' ], SRFM_VER, true );
+					endif;
 			endif;
 
-			if ( 'cf-turnstile' === $this->captcha_security_type ) :
-				// Cloudflare Turnstile script.
-				wp_enqueue_script( // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
-					SRFM_SLUG . '-cf-turnstile',
-					'https://challenges.cloudflare.com/turnstile/v0/api.js',
-					[],
-					null,
-					[
-						false,
-						'defer' => true,
-					]
-				);
+				if ( 'cf-turnstile' === $this->captcha_security_type ) :
+					// Cloudflare Turnstile script.
+					wp_enqueue_script( // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+						SRFM_SLUG . '-cf-turnstile',
+						'https://challenges.cloudflare.com/turnstile/v0/api.js',
+						[],
+						null,
+						[
+							false,
+							'defer' => true,
+						]
+					);
 			endif;
-			if ( 'hcaptcha' === $this->captcha_security_type ) :
-				wp_enqueue_script( 'hcaptcha', 'https://js.hcaptcha.com/1/api.js', [], null, [ 'strategy' => 'defer' ] ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+				if ( 'hcaptcha' === $this->captcha_security_type ) :
+					wp_enqueue_script( 'hcaptcha', 'https://js.hcaptcha.com/1/api.js', [], null, [ 'strategy' => 'defer' ] ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
 			endif;
-			?>
+				?>
 				<button style="<?php echo $this->btn_from_theme ? '' : ' font-family: inherit; font-weight: var(--wp--custom--font-weight--medium); line-height: normal;'; ?>width:100%;" id="srfm-submit-btn" class="<?php echo esc_attr( 'v2-invisible' === $this->recaptcha_version || 'v3-reCAPTCHA' === $this->recaptcha_version ? 'g-recaptcha ' : '' ); ?> <?php echo esc_attr( '1' === $this->btn_from_theme ? 'wp-block-button__link' : 'srfm-button srfm-submit-button srfm-btn-frontend srfm-custom-button' ); ?> " <?php echo 'v2-invisible' === $this->recaptcha_version || 'v3-reCAPTCHA' === $this->recaptcha_version ? esc_attr( 'recaptcha-type=' . $this->recaptcha_version . ' data-sitekey=' . $this->google_captcha_site_key ) : ''; ?>>
 					<div class="srfm-submit-wrap">
 						<?php echo esc_html( $this->button_text ); ?>
@@ -221,9 +221,9 @@ class Inlinebutton_Markup extends Base {
 					</div>
 				</button>
 			</div>
-			<?php
+				<?php
 		endif;
-		return ob_get_clean();
-	}
+			return ob_get_clean();
+		}
 
 }
