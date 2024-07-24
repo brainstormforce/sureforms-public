78e43fe30ffc88b8c8e61ad3fa9f2bae8ee175ee
Vansh <kvansh297@gmail.com>
2024-07-24 13:49:30 +0530

    checks fix

--- src/blocks/components/default.js
+++ src/blocks/components/default.js
@@ -35,7 +35,7 @@ export const CheckboxComponent = ( {
 			>
 				<span className="srfm-span-wrap">
 					<svg className="srfm-check-icon" width="12px" height="10px">
-						<svg class="srfm-inline-svg" viewBox="0 0 12 10">
+						<svg className="srfm-inline-svg" viewBox="0 0 12 10">
 							<polyline points="1.5 6 4.5 9 10.5 1"></polyline>
 						</svg>
 					</svg>
