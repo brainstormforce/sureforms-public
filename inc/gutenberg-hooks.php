9c5e0bac4959b6cd4c1d811495864b10d83ee707
Vansh <kvansh297@gmail.com>
2024-07-24 18:31:16 +0530

    changes block name to slider

--- inc/gutenberg-hooks.php
+++ inc/gutenberg-hooks.php
@@ -54,7 +54,7 @@ class Gutenberg_Hooks {
 		// pro blocks.
 		'srfm/date-time-picker',
 		'srfm/hidden',
-		'srfm/number-slider',
+		'srfm/slider',
 		'srfm/password',
 		'srfm/rating',
 		'srfm/upload',
