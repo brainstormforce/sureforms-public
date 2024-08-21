42d6571704f165ce6ea1072eaba1cc7eadb9d6c3
osk02 <omk@bsf.io>
2024-08-21 15:35:31 +0530

    removed slider vars and added sizing filter

--- inc/generate-form-markup.php
+++ inc/generate-form-markup.php
@@ -232,21 +232,12 @@ class Generate_Form_Markup {
 					--srfm-dropdown-placeholder-color: rgba( from <?php echo esc_html( $help_color_var ); ?> r g b / 0.50 );
 					--srfm-dropdown-icon-color: rgba( from <?php echo esc_html( $help_color_var ); ?> r g b / 0.65 );
 					--srfm-dropdown-icon-disabled: rgba( from <?php echo esc_html( $help_color_var ); ?> r g b / 0.25 );
-					
-					/* Slider Variables -> Temporary, will be moved later. */
-					--srfm-slider-label-font-size: 12px;
-					--srfm-slider-label-font-weight: 400;
-					--srfm-slider-label-line-height: 16px;
-					--srfm-slider-label-top-padding: 6px;
-					--srfm-slider-label-color: hsl( from <?php echo esc_html( $help_color_var ); ?> h s l / 0.50 );
-					--srfm-slider-thumb-shadow-color: hsl( from <?php echo esc_html( $help_color_var ); ?> h s l / 0.10 );
-					--srfm-slider-tooltip-shadow-color: hsl( from <?php echo esc_html( $help_color_var ); ?> h s l / 0.08 );
 					<?php
 					// Echo the CSS variables for the form according to the field spacing selected.
 					foreach ( $selected_size as $variable => $value ) {
 						echo esc_html( Helper::get_string_value( $variable ) ) . ': ' . esc_html( Helper::get_string_value( $value ) ) . ';';
 					}
-					do_action( 'srfm_form_css_variables', $id );
+					do_action( 'srfm_form_css_variables', $id, $primary_color_var, $help_color_var );
 					// echo custom css on page/post.
 					if ( 'sureforms_form' !== $current_post_type ) :
 						echo wp_kses_post( $custom_css );
