bdd3c24aa7010bb073d87458fd6e8ef0dbc532f2
osk02 <omk@bsf.io>
2024-08-26 18:22:47 +0530

    initial focus on title bar

--- src/admin/single-form-settings/components/SRFMEditorHeader.js
+++ src/admin/single-form-settings/components/SRFMEditorHeader.js
@@ -2,6 +2,7 @@ import { __ } from '@wordpress/i18n';
 import { useEntityProp } from '@wordpress/core-data';
 import { TextControl } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
+import { useEffect, useRef } from '@wordpress/element';
 
 const SRFMEditorHeader = () => {
 	const postId = useSelect( ( select ) => {
@@ -15,8 +16,16 @@ const SRFMEditorHeader = () => {
 		postId
 	);
 
+	const formTitleInputRef = useRef( null );
+	useEffect( () => {
+		if ( formTitleInputRef.current ) {
+			formTitleInputRef.current.focus();
+		}
+	}, [] );
+
 	return (
 		<TextControl
+			ref={ formTitleInputRef }
 			className="srfm-header-title-input"
 			placeholder={ __( 'Form Title', 'sureforms' ) }
 			value={ title }
