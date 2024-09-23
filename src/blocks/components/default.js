1f80471dff9f48dcac7610e26fba60307e4e0667
Om Kolte <omk@bsf.io>
2024-09-23 15:40:14 +0530

    QA fix for helptext

--- src/blocks/components/default.js
+++ src/blocks/components/default.js
@@ -24,44 +24,53 @@ export const CheckboxComponent = ( {
 	}
 
 	return (
-		<div className="srfm-block-wrap">
-			<input
-				type="checkbox"
-				checked={ selected }
-				className={ `srfm-input-common screen-reader-text srfm-input-checkbox ${ inputClassname }` }
-			/>
-			<label
-				className="srfm-cbx"
-				htmlFor={ `srfm-checkbox-${ blockID }` }
-			>
-				<span className="srfm-span-wrap">
-					<svg className="srfm-check-icon" width="12px" height="10px">
-						<svg className="srfm-inline-svg" viewBox="0 0 12 10">
-							<polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-						</svg>
-					</svg>
-				</span>
-				<span
-					className={ `srfm-span-wrap srfm-block-label ${ isRequired }` }
+		<>
+			<div className="srfm-block-wrap">
+				<input
+					type="checkbox"
+					checked={ selected }
+					className={ `srfm-input-common screen-reader-text srfm-input-checkbox ${ inputClassname }` }
+				/>
+				<label
+					className="srfm-cbx"
+					htmlFor={ `srfm-checkbox-${ blockID }` }
 				>
-					<RichText
-						tagName="label"
-						value={ label }
-						onChange={ ( value ) => {
-							setAttributes( {
-								label: decodeHtmlEntities( value ),
-							} );
-						} }
-						multiline={ false }
-						id={ blockID }
-					/>
-				</span>
-			</label>
+					<span className="srfm-span-wrap">
+						<svg
+							className="srfm-check-icon"
+							width="12px"
+							height="10px"
+						>
+							<svg
+								className="srfm-inline-svg"
+								viewBox="0 0 12 10"
+							>
+								<polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+							</svg>
+						</svg>
+					</span>
+					<span
+						className={ `srfm-span-wrap srfm-block-label ${ isRequired }` }
+					>
+						<RichText
+							tagName="label"
+							value={ label }
+							onChange={ ( value ) => {
+								setAttributes( {
+									label: decodeHtmlEntities( value ),
+								} );
+							} }
+							multiline={ false }
+							id={ blockID }
+						/>
+					</span>
+				</label>
+			</div>
 			<HelpText
 				help={ help }
 				setAttributes={ setAttributes }
 				block_id={ blockID }
 			/>
-		</div>
+		</>
 	);
 };
