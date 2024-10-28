bcea61fe42eb27e3bee4aef7861ec9b400db8c62
Aditya Jain <adi3890@gmail.com>
2024-10-28 18:59:55 +0530

    fixing linting issues

--- images/Logo.js
+++ images/Logo.js
@@ -1,18 +1,42 @@
 export default () => {
 	return (
-		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<g clip-path="url(#clip0_15961_16935)">
-				<rect width="24" height="24" fill="#030712"/>
-				<path d="M6.85742 5.14258H17.1431V8.57115H8.57171L6.85742 10.2854V8.57115V5.14258Z" fill="white"/>
-				<path d="M6.85742 5.14258H17.1431V8.57115H8.57171L6.85742 10.2854V8.57115V5.14258Z" fill="white"/>
-				<path d="M6.85742 10.2852H15.4288V13.7137H8.57171L6.85742 15.428V13.7137V10.2852Z" fill="white"/>
-				<path d="M6.85742 10.2852H15.4288V13.7137H8.57171L6.85742 15.428V13.7137V10.2852Z" fill="white"/>
-				<path d="M6.85742 15.4287H12.0003V18.8573H6.85742V15.4287Z" fill="white"/>
-				<path d="M6.85742 15.4287H12.0003V18.8573H6.85742V15.4287Z" fill="white"/>
+		<svg
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<g clipPath="url(#clip0_15961_16935)">
+				<rect width="24" height="24" fill="#030712" />
+				<path
+					d="M6.85742 5.14258H17.1431V8.57115H8.57171L6.85742 10.2854V8.57115V5.14258Z"
+					fill="white"
+				/>
+				<path
+					d="M6.85742 5.14258H17.1431V8.57115H8.57171L6.85742 10.2854V8.57115V5.14258Z"
+					fill="white"
+				/>
+				<path
+					d="M6.85742 10.2852H15.4288V13.7137H8.57171L6.85742 15.428V13.7137V10.2852Z"
+					fill="white"
+				/>
+				<path
+					d="M6.85742 10.2852H15.4288V13.7137H8.57171L6.85742 15.428V13.7137V10.2852Z"
+					fill="white"
+				/>
+				<path
+					d="M6.85742 15.4287H12.0003V18.8573H6.85742V15.4287Z"
+					fill="white"
+				/>
+				<path
+					d="M6.85742 15.4287H12.0003V18.8573H6.85742V15.4287Z"
+					fill="white"
+				/>
 			</g>
 			<defs>
 				<clipPath id="clip0_15961_16935">
-					<rect width="24" height="24" fill="white"/>
+					<rect width="24" height="24" fill="white" />
 				</clipPath>
 			</defs>
 		</svg>
