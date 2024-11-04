e918b79be895c8904faac8bf278178f4cdb7a0b3
mohitbsftester <mohits@bsf.io>
2024-11-05 02:12:41 +0530

    Common Login File with browser close

--- tests/play/specs/sureforms.spec.js
+++ tests/play/specs/sureforms.spec.js
@@ -1,32 +1,31 @@
 const { test, expect } = require('@playwright/test');
+const { loginAsAdmin } = require('./util.js'); 
 
-test.describe('My WordPress Plugin', () => {
+test.describe('Deactivate and activate the plugin', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('/wp-login.php');
-    await page.fill('#user_login', 'admin');
-    await page.fill('#user_pass', 'password');
-    await page.click('#wp-submit');
-    await page.goto('/wp-admin');
+    // Use the imported login function
+    await loginAsAdmin(page);
   });
+
 
   test('deactivate the plugin', async ({ page }) => {
     await page.goto('/wp-admin/plugins.php');
 
-	// Deactivate plugin if it's already active
+    // Deactivate plugin if it's already active
     const isPluginActive = await page.isVisible('#deactivate-sureforms');
     if (isPluginActive) {
       await page.click('#deactivate-sureforms');
       await page.waitForSelector('#activate-sureforms', { timeout: 5000 });
     }
 
-	// Click the activate button and wait for navigation
+    // Click the activate button and wait for navigation
     await Promise.all([
-		page.click('#activate-sureforms', { timeout: 5000 }),
-		page.waitForNavigation({ waitUntil: 'networkidle' }),
-	]);
+      page.click('#activate-sureforms', { timeout: 5000 }),
+      page.waitForNavigation({ waitUntil: 'networkidle' }),
+    ]);
 
-	// Check if the expected success message or element is visible after activation
-	const isActive = await page.isVisible(`text="Get started with SureForms"`);
-	expect(isActive).toBeTruthy();
+    // Check if the expected success message or element is visible after activation
+    const isActive = await page.isVisible(`text="Get started with SureForms"`);
+    expect(isActive).toBeTruthy();
   });
 });
