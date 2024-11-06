f0e29c065b8718a022e22770d00db7e75f83e946
Rahul Verma <rahulvarma722@gmail.com>
2024-11-06 16:44:39 +0530

    address field completed

--- inc/fields/address-markup.php
+++ inc/fields/address-markup.php
@@ -41,7 +41,7 @@ class Address_Markup extends Base {
 	public function markup( $content = '' ) {
 		ob_start(); ?>
 			<div data-block-id="<?php echo esc_attr( $this->block_id ); ?>" class="srfm-block-single srfm-block srfm-<?php echo esc_attr( $this->slug ); ?>-block srf-<?php echo esc_attr( $this->slug ); ?>-<?php echo esc_attr( $this->block_id ); ?>-block<?php echo esc_attr( $this->block_width ); ?><?php echo esc_attr( $this->class_name ); ?> <?php echo esc_attr( $this->conditional_class ); ?>">
-				<fieldset>
+				<fieldset role="group" tabindex="0">
 					<legend class="srfm-block-legend">
 						<?php echo wp_kses_post( $this->label_markup ); ?>
 						<?php echo wp_kses_post( $this->help_markup ); ?>
