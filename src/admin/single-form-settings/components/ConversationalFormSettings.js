2442ca9c924fc07349386c4ebdc37e99ce97bfa4
Vansh <kvansh297@gmail.com>
2024-11-20 13:11:04 +0530

    fix: conversational component logic

--- src/admin/single-form-settings/components/ConversationalFormSettings.js
+++ src/admin/single-form-settings/components/ConversationalFormSettings.js
@@ -10,62 +10,59 @@ const ConversationalFormSettings = () => {
 		showSwitch
 	);
 
-	const isBusinessPlan = srfm_admin?.pro_plugin_name.split( ' ' )[ 1 ] === 'Business';
-
 	return (
-		<div
-			style={ {
-				display: 'flex',
-				flexDirection: 'column',
-				gap: '8px',
-			} }>
-			<div
-				style={ {
-					display: 'flex',
-					justifyContent: 'space-between',
-					alignItems: 'center',
-				} }
-			>
-				<span> { __( 'Conversational Layout', 'sureforms' ) } </span>
-				{!isBusinessPlan&&<span
+		<>
+			{ false === switchConversationalForms
+				? <div
 					style={ {
-						minWidth: '16px',
-						maxHeight: '16px',
-						padding: '2px',
-						gap: '2px',
-						borderRadius: '4px',
-						background: '#1F2937',
-						color: '#fff',
-						border: '0.5px solid #374151',
-						fontSize: '9px',
-						fontWeight: '600',
-						lineHeight: 'normal',
-						textAlign: 'center',
-					} }
-				>{ __( 'Business Plan', 'sureforms' ) }</span>}
-				{ false === switchConversationalForms ? <InstantFormToggle
-					disabled={ true }
-				/> : switchConversationalForms }
-			</div>
-			{!isBusinessPlan&&<div
-				style={ {
-					padding: '4px',
-					borderRadius: '6px',
-					background: '#F0F9FF',
-					color: '#1E293B',
-					border: '0.5px solid #BAE6FD',
-					fontSize: '13px',
-					fontWeight: '400',
-					lineHeight: '20px',
-					textAlign: 'left',
-				} }
-			>
-				{ __( 'To use this feature you need to upgrade to the Business plan. ', 'sureforms' ) }
-				<ExternalLink href="https://sureforms.com/pricing" target="_blank">
-					{ __( 'Upgrade now', 'sureforms' ) }
-				</ExternalLink>
-			</div>}
-		</div>
+						display: 'flex',
+						flexDirection: 'column',
+						gap: '8px',
+					} }>
+					<div
+						style={ {
+							display: 'flex',
+							justifyContent: 'space-between',
+							alignItems: 'center',
+						} }
+					>
+						<span> { __( 'Conversational Layout', 'sureforms' ) } </span>
+						<span
+							style={ {
+								minWidth: '16px',
+								maxHeight: '16px',
+								padding: '2px',
+								gap: '2px',
+								borderRadius: '4px',
+								background: '#1F2937',
+								color: '#fff',
+								border: '0.5px solid #374151',
+								fontSize: '9px',
+								fontWeight: '600',
+								lineHeight: 'normal',
+								textAlign: 'center',
+							} }
+						>{ __( 'Business Plan', 'sureforms' ) }</span><InstantFormToggle disabled={ true } /></div>
+					<div
+						style={ {
+							padding: '4px',
+							borderRadius: '6px',
+							background: '#F0F9FF',
+							color: '#1E293B',
+							border: '0.5px solid #BAE6FD',
+							fontSize: '13px',
+							fontWeight: '400',
+							lineHeight: '20px',
+							textAlign: 'left',
+						} }
+					>
+						{ __( 'To use this feature you need to upgrade to the Business plan. ', 'sureforms' ) }
+						<ExternalLink href="https://sureforms.com/pricing" target="_blank">
+							{ __( 'Upgrade now', 'sureforms' ) }
+						</ExternalLink>
+					</div>
+				</div> : switchConversationalForms }
+		</>
 	);
 };
 
