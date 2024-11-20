d01e87f315d745a26aca9670be475442e8a74c02
Rahul Verma <rahulvarma722@gmail.com>
2024-11-20 14:32:26 +0530

    Rearraged block order

--- plugin-loader.php
+++ plugin-loader.php
@@ -261,11 +261,6 @@ class Plugin_Loader {
 	 * @return void
 	 */
 	public function load_plugin() {
-		/**
-		 * Load core files necessary for the plugin.
-		 * This method is called in the plugin loader to ensure core files are loaded during the 'plugins_loaded' action.
-		 */
-		$this->load_core_files();
 		Post_Types::get_instance();
 		Form_Submit::get_instance();
 		Block_Patterns::get_instance();
@@ -292,6 +287,11 @@ class Plugin_Loader {
 		AI_Auth::get_instance();
 		Updater::get_instance();
 		DatabaseRegister::init();
+		/**
+		 * Load core files necessary for the plugin.
+		 * This method is called in the plugin loader to ensure core files are loaded during the 'plugins_loaded' action.
+		 */
+		$this->load_core_files();
 	}
 
 	/**
