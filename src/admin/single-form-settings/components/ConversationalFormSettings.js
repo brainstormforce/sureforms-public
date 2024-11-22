be9b1bad520bd321b8d8ebc2225eef12b5a3eae4
Vansh <kvansh297@gmail.com>
2024-11-22 16:23:41 +0530

    removed inline styles

--- src/admin/single-form-settings/components/ConversationalFormSettings.js
+++ src/admin/single-form-settings/components/ConversationalFormSettings.js
@@ -1,65 +1,48 @@
 import { __ } from '@wordpress/i18n';
-import { InstantFormToggle } from '../InstantForm';
-import { ExternalLink } from '@wordpress/components';
+import { ExternalLink, FormToggle } from '@wordpress/components';
 import { applyFilters } from '@wordpress/hooks';
 
 const ConversationalFormSettings = () => {
 	const showComponent = false;
-	const switchConversationalForms = applyFilters(
+	const conversationalFormSettings = applyFilters(
 		'srfm.instant_form_settings.conversational_forms',
 		showComponent
 	);
 
 	return (
 		<>
-			{ false === switchConversationalForms
-				? <div
-					className='srfm-conversational-form-settings-ctn'
-					>
+			{ false === conversationalFormSettings
+				? <>
+					<div className="srfm-instant-form-settings srfm-instant-form-settings-inline">
+						<div className="srfm-conversational-placeholder-label-ctn">
+							<label
+								htmlFor="srfm-conversational-placeholder-toggle"
+								style={ { cursor: 'pointer' } }
+							>
+								{ __( 'Conversational Layout', 'sureforms' ) }
+							</label>
+							<span
+								className="srfm-conversational-placeholder-label"
+							>
+								{ __( 'Pro', 'sureforms' ) }
+							</span>
+						</div>
+						<FormToggle
+							disabled={ true }
+							label=""
+							id="srfm-conversational-placeholder-toggle"
+						/>
+					</div>
 					<div
-						style={ {
-							display: 'flex',
-							justifyContent: 'space-between',
-							alignItems: 'center',
-						} }
-						className='srfm-conversational-form-settings-'
-					>
-						<span> { __( 'Conversational Layout', 'sureforms' ) } </span>
-						<span
-							style={ {
-								minWidth: '16px',
-								maxHeight: '16px',
-								padding: '2px',
-								gap: '2px',
-								borderRadius: '4px',
-								background: '#1F2937',
-								color: '#fff',
-								border: '0.5px solid #374151',
-								fontSize: '9px',
-								fontWeight: '600',
-								lineHeight: 'normal',
-								textAlign: 'center',
-							} }
-						>{ __( 'Business Plan', 'sureforms' ) }</span><InstantFormToggle disabled={ true } /></div>
-					<div
-						style={ {
-							padding: '4px',
-							borderRadius: '6px',
-							background: '#F0F9FF',
-							color: '#1E293B',
-							border: '0.5px solid #BAE6FD',
-							fontSize: '13px',
-							fontWeight: '400',
-							lineHeight: '20px',
-							textAlign: 'left',
-						} }
+						className="srfm-conversational-placeholder-help-text"
 					>
 						{ __( 'To use this feature you need to upgrade to the Business plan. ', 'sureforms' ) }
 						<ExternalLink href="https://sureforms.com/pricing" target="_blank">
 							{ __( 'Upgrade now', 'sureforms' ) }
 						</ExternalLink>
 					</div>
-				</div> : switchConversationalForms }
+				</>
+			 : conversationalFormSettings }
 		</>
 	);
 };
