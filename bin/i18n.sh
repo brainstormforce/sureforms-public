e02ed81dc903bf6e24bb64878839bbfd49fc363c
Aditya Jain <adi3890@gmail.com>
2024-12-13 22:51:23 +0530

    adding translations for German, Spanish

--- bin/i18n.sh
+++ bin/i18n.sh
@@ -15,8 +15,8 @@ npm run i18n:po
 echo "Translating PO files using GPT-PO..."
 npm run i18n:gptpo:nl
 npm run i18n:gptpo:fr
-# npm run i18n:gptpo:de
-# npm run i18n:gptpo:es
+npm run i18n:gptpo:de
+npm run i18n:gptpo:es
 # npm run i18n:gptpo:pt
 # npm run i18n:gptpo:pl
 # npm run i18n:gptpo:it
