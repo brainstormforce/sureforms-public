94d601468524130eeb18275b6ba766ceafe8ba2c
Brainstorm Force <brainstormforce@Garvits-MacBook-Air.local>
2024-12-20 09:12:55 +0545

    Saving changes

--- admin/views/single-entry.php
+++ admin/views/single-entry.php
@@ -113,7 +113,7 @@ class Single_Entry {
 							<?php $this->render_form_data( $meta_data, $excluded_fields ); ?>
 						</div>
 						<div id="postbox-container-3" class="postbox-container">
-							<?php $this->render_entry_logs(); ?>
+							<?php $this->render_entry_logs( $this->entry['logs'] ); ?>
 						</div>
 					</div><!-- /post-body -->
 					<br class="clear">
@@ -129,166 +129,6 @@ class Single_Entry {
 				do_action( 'srfm_before_entry_form_closing_tag', $this->entry, $this );
 				?>
 			</form>
-		</div>
-		<?php
-	}
-
-	/**
-	 * Helper method to paginate the provided array data.
-	 *
-	 * @param array<mixed> $array Array item to paginate.
-	 * @param int          $current_page Current page number.
-	 * @param int          $items_per_page Total items to return per pagination.
-	 * @since x.x.x
-	 * @return array<mixed>
-	 */
-	public static function paginate_array( $array, $current_page, $items_per_page = 3 ) {
-		$total_items = count( $array );
-		$total_pages = Helper::get_integer_value( ceil( $total_items / $items_per_page ) );
-
-		// Ensure current page is within bounds.
-		$current_page = max( 1, min( $total_pages, $current_page ) );
-
-		// Calculate the offset for slicing.
-		$offset = ( $current_page - 1 ) * $items_per_page;
-
-		// Get the items for the current page.
-		$items = array_slice( $array, $offset, $items_per_page, true );
-
-		// Determine the next and previous page numbers.
-		$next_page = $current_page < $total_pages ? $current_page + 1 : false;
-		$prev_page = $current_page > 1 ? $current_page - 1 : false;
-
-		return compact(
-			'items',
-			'offset',
-			'next_page',
-			'prev_page',
-			'total_items',
-			'total_pages',
-			'current_page',
-		);
-	}
-
-	/**
-	 * Prints entry note item markup.
-	 *
-	 * @param array $note Single note array.
-	 * @since x.x.x
-	 * @return void
-	 */
-	public static function entry_note_item_markup( $note ) {
-		$submitted_by_user = get_user_by( 'ID', absint( $note['submitted_by'] ) )->display_name;
-		?>
-		<div class="entry-note-item">
-			<strong class="entry-log-title">
-				<?php
-					/* translators: Here %s means the users display name. */
-					printf( esc_html__( 'Submitted by %s', 'sureforms' ), esc_html( $submitted_by_user ) );
-				?>
-			</strong>
-			<br/>
-			<small><?php echo esc_html( gmdate( 'Y-m-d H:i:s', $note['timestamp'] ) ); ?></small>
-			<?php echo wp_kses_post( wpautop( $note['note'] ) ); ?>
-		</div>
-		<?php
-	}
-
-	/**
-	 * Provides table markup for the entry logs.
-	 *
-	 * @param array<mixed> $entry_logs Entry logs stored in the database.
-	 * @since x.x.x
-	 * @return void
-	 */
-	public static function entry_logs_table_markup( $entry_logs ) {
-		if ( empty( $entry_logs ) ) {
-			?>
-			<p class="no-logs-found"><?php esc_html_e( 'No logs found for this entry.', 'sureforms' ); ?></p>
-			<?php
-			return; // Bail early if entry not found.
-		}
-		?>
-		<table class="striped entry-logs-table">
-			<tbody>
-				<?php foreach ( $entry_logs as $key => $log ) { ?>
-					<tr>
-						<td class="entry-log-container">
-							<div class="entry-log">
-								<div class="info-wrap">
-									<h4 class="entry-log-title">
-										<?php echo esc_html( $log['title'] ); ?>
-										<?php echo esc_html( gmdate( '\a\t Y-m-d H:i:s', $log['timestamp'] ) ); ?>
-									</h4>
-									<div class="entry-log-messages">
-									<?php foreach ( $log['messages'] as $message ) { ?>
-										<p><?php echo wp_kses_post( $message ); ?></p>
-									<?php } ?>
-									</div>
-								</div>
-								<button type="button" class="btn-delete-log" data-log-key="<?php echo esc_attr( $key ); ?>">
-									<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path d="M12.2837 7.5L11.9952 15M8.00481 15L7.71635 7.5M16.023 4.82547C16.308 4.86851 16.592 4.91456 16.875 4.96358M16.023 4.82547L15.1332 16.3938C15.058 17.3707 14.2434 18.125 13.2637 18.125H6.73631C5.75655 18.125 4.94198 17.3707 4.86683 16.3938L3.97696 4.82547M16.023 4.82547C15.0677 4.6812 14.1013 4.57071 13.125 4.49527M3.125 4.96358C3.40798 4.91456 3.69198 4.86851 3.97696 4.82547M3.97696 4.82547C4.93231 4.6812 5.89874 4.57071 6.875 4.49527M13.125 4.49527V3.73182C13.125 2.74902 12.3661 1.92853 11.3838 1.8971C10.9244 1.8824 10.463 1.875 10 1.875C9.53696 1.875 9.07565 1.8824 8.61618 1.8971C7.63388 1.92853 6.875 2.74902 6.875 3.73182V4.49527M13.125 4.49527C12.0938 4.41558 11.0516 4.375 10 4.375C8.94836 4.375 7.9062 4.41558 6.875 4.49527" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>
-								</button>
-							</div>
-						</td>
-					</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-		<?php
-	}
-
-	/**
-	 * Render the entry notes for the specific entry.
-	 *
-	 * @since x.x.x
-	 */
-	private function render_entry_notes() {
-		$notes = $this->entry['notes'];
-		?>
-		<div id="submitdiv" class="postbox entry-notes">
-			<div class="in-progress-overlay">
-				<svg width="45" height="45" fill="#2271b1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/><path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z" class="spinner_z9k8"/></svg>
-			</div>
-			<div class="postbox-header">
-				<h2><?php esc_html_e( 'Entry Notes', 'sureforms' ); ?></h2>
-				<button type="button" id="srfm-add-entry-note" class="srfm-add-entry-note-button <?php echo empty( $notes ) ? 'hidden' : ''; ?>">
-					<?php esc_html_e( 'Add Note', 'sureforms' ); ?>
-					<svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M8 3.33594V12.6693' stroke='black' stroke-width='1.25' stroke-linecap='round' stroke-linejoin='round'/><path d='M3.33337 8H12.6667' stroke='black' stroke-width='1.25' stroke-linecap='round' stroke-linejoin='round'/></svg>
-				</button>
-			</div>
-			<div class="inside">
-				<div class="srfm-entry-note-wrapper">
-					<div class="entry-notes-container">
-						<?php // We will populate this div using AJAX. ?>
-					</div>
-					<div class="add-notes-field <?php echo ! empty( $notes ) ? 'hidden' : ''; ?>">
-						<textarea id="srfm-entry-note" rows="5"></textarea>
-						<button id="srfm-add-note" type="submit" class="button" data-entry-id=<?php echo esc_html( $this->entry_id ); ?>><?php esc_html_e( 'Submit Note', 'sureforms' ); ?></button>
-					</div>
-				</div>
-			</div>
-
-			<div class="entry-navigation-btn-wrapper">
-				<button type="button" class="entry-navigation-btn entry-notes-nav-btn" data-type="prev">
-					<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M10 12L6 8L10 4" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-					</svg>
-					<span>
-						<?php esc_html_e( 'Previous', 'sureforms' ); ?>
-					</span>
-				</button>
-				<button type="button" class="entry-navigation-btn entry-notes-nav-btn" data-type="next">
-					<span>
-						<?php esc_html_e( 'Next', 'sureforms' ); ?>
-					</span>
-					<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M6 12L10 8L6 4" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-					</svg>
-				</button>
-			</div>
 		</div>
 		<?php
 	}
@@ -506,47 +346,52 @@ class Single_Entry {
 		</div>
 		<?php
 	}
-
 	/**
 	 * Render the entry logs for a specific entry.
 	 *
+	 * @param array<mixed> $entry_logs Entry logs stored in the database.
 	 * @since 0.0.13
-	 * @since x.x.x - Removed $entry_logs parameter.
 	 * @return void
 	 */
-	private function render_entry_logs() {
+	private function render_entry_logs( $entry_logs ) {
+		ob_start();
 		?>
-		<div id="sureform_entry_meta" class="postbox srfm-entry-logs">
-			<div class="in-progress-overlay">
-				<svg width="45" height="45" fill="#2271b1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/><path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z" class="spinner_z9k8"/></svg>
-			</div>
+		<div id="sureform_entry_meta" class="postbox">
 			<div class="postbox-header">
 				<!-- Removed "hndle ui-sortable-handle" class from h2 to remove the draggable stylings. -->
 				<h2><?php esc_html_e( 'Entry Logs', 'sureforms' ); ?></h2>
 				<!-- <div class="handle-actions hide-if-no-js"><button type="button" class="handle-order-higher" aria-disabled="false" aria-describedby="sureform_entry_meta-handle-order-higher-description"><span class="screen-reader-text">Move up</span><span class="order-higher-indicator" aria-hidden="true"></span></button><span class="hidden" id="sureform_entry_meta-handle-order-higher-description">Move Form Data box up</span><button type="button" class="handle-order-lower" aria-disabled="false" aria-describedby="sureform_entry_meta-handle-order-lower-description"><span class="screen-reader-text">Move down</span><span class="order-lower-indicator" aria-hidden="true"></span></button><span class="hidden" id="sureform_entry_meta-handle-order-lower-description">Move Form Data box down</span><button type="button" class="handlediv" aria-expanded="true"><span class="screen-reader-text">Toggle panel: Form Data</span><span class="toggle-indicator" aria-hidden="true"></span></button></div> -->
 			</div>
 			<div class="inside">
-				<?php // We will populate this div using AJAX. ?>
-			</div>
-			<div class="entry-navigation-btn-wrapper">
-				<button type="button" class="entry-navigation-btn entry-logs-navigation-btn" data-type="prev">
-					<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M10 12L6 8L10 4" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-					</svg>
-					<span>
-						<?php esc_html_e( 'Previous', 'sureforms' ); ?>
-					</span>
-				</button>
-				<button type="button" class="entry-navigation-btn entry-logs-navigation-btn" data-type="next">
-					<span>
-						<?php esc_html_e( 'Next', 'sureforms' ); ?>
-					</span>
-					<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M6 12L10 8L6 4" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-					</svg>
-				</button>
+				<table class="widefat striped entry-logs-table">
+					<tbody>
+						<?php if ( ! empty( $entry_logs ) ) { ?>
+								<?php foreach ( $entry_logs as $log ) { ?>
+									<tr>
+										<td class="entry-log-container">
+											<div class="entry-log">
+												<h4 class="entry-log-title">
+													<?php echo esc_html( $log['title'] ); ?>
+													<?php echo esc_html( gmdate( '\a\t Y-m-d H:i:s', $log['timestamp'] ) ); ?>
+												</h4>
+												<div class="entry-log-messages">
+												<?php foreach ( $log['messages'] as $message ) { ?>
+													<p><?php echo wp_kses_post( $message ); ?></p>
+												<?php } ?>
+												</div>
+											</div>
+										</td>
+									</tr>
+								<?php } ?>
+						<?php } else { ?>
+							<p><?php esc_html_e( 'No logs found for this entry.', 'sureforms' ); ?></p>
+						<?php } ?>
+					</tbody>
+				</table>
 			</div>
 		</div>
 		<?php
+		$content = ob_get_clean();
+		echo apply_filters( 'srfm_entry_logs_markup', $content, $entry_logs ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
