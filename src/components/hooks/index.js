a5379d56ec5c6240a177b43595ddfcefc74f47c3
Rahul Verma <rahulvarma722@gmail.com>
2024-12-30 18:48:49 +0530

    rename filter

--- src/components/hooks/index.js
+++ src/components/hooks/index.js
@@ -1,5 +1,5 @@
 import { applyFilters } from '@wordpress/hooks';
 
-export const AfterAttributesPanelBody = ( options, props ) => {
+export const attributeOptionsWithFilter = ( options, props ) => {
 	return applyFilters( 'srfm.block.attributes.panel.body', options, props );
 };
