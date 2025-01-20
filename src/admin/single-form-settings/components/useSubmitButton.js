02294c4a110e435f568c5478767595e32195b0e5
Rahul Verma <rahulvarma722@gmail.com>
2025-01-20 07:11:42 +0530

    Modify branch according to optimization

--- src/admin/single-form-settings/components/useSubmitButton.js
+++ src/admin/single-form-settings/components/useSubmitButton.js
@@ -5,21 +5,14 @@ import { createRoot } from 'react-dom/client';
 import { RichText } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import { BlockInserterWrapper } from '../Inserter.js';
-import { shouldSubmitButtonDisable } from '@Components/hooks';
 
 const SubmitButton = ( props ) => {
 	const metaValues = useSelect( ( select ) => select( editorStore ).getEditedPostAttribute( 'meta' ) );
-
 	const buttonElement = useRef( null );
-	const disableSubmitButton = shouldSubmitButtonDisable( metaValues );
 
 	const labelText = metaValues?._srfm_submit_button_text || __( 'Submit', 'sureforms' );
 
 	useEffect( () => {
-		if ( disableSubmitButton ) {
-			return;
-		}
-
 		const eventToButton = () => {
 			// need multiple timeouts for DOM elements to find.
 			// click on form section
@@ -46,11 +39,7 @@ const SubmitButton = ( props ) => {
 				buttonElement.current.removeEventListener( 'click', eventToButton );
 			}
 		};
-	}, [ disableSubmitButton ] );
-
-	if ( disableSubmitButton ) {
-		return null;
-	}
+	}, [] );
 
 	return <button className="srfm-submit-richtext srfm-button srfm-submit-button srfm-btn-alignment srfm-btn-bg-color" ref={ buttonElement }>
 		<RichText
--- src/components/hooks/index.js
+++ src/components/hooks/index.js
@@ -15,15 +15,3 @@ export const enhanceMultiChoiceOptions = ( option, args ) => {
 		args
 	);
 };
-
-export const submitButtonContainerOptions = ( options, args ) => {
-	return applyFilters(
-		'srfm.submit.button.container.options',
-		options,
-		args
-	);
-};
-
-export const shouldSubmitButtonDisable = ( args ) => {
-	return applyFilters( 'srfm.submit.button.disable', true, args );
-};
