94ef0229e18f379695bfcfc913b1b6af3624fcb0
Rahul Verma <rahulvarma722@gmail.com>
2025-02-10 17:46:08 +0530

    implementing force ui

--- src/admin/dashboard/Dashboard.js
+++ src/admin/dashboard/Dashboard.js
@@ -7,12 +7,26 @@ import { Fragment } from '@wordpress/element';
 import GetStarted from './GetStarted';
 import Overview from './Overview';
 import DashboardModel from './templates/DashboardModel';
+import { Container, Topbar, Badge, Title } from '@bsf/force-ui';
 
 export default () => {
 	return (
 		<>
 			<DashboardModel>
 				<Fragment>
+					<h1 className='border-x flex justify-between items-center'>Hello</h1>
+					<Title
+						description=""
+						icon={null}
+						iconPosition="right"
+						size="sm"
+						tag="h2"
+						title="Basic Title"
+						className="srfm-dashboard-title border-b"
+					/>
+
+					<Badge label="-- badge force ui Pro" size="xs" variant="neutral" closable={false} />
+					
 					<GetStarted />
 					<ScFlex
 						style={ { '--sc-flex-column-gap': '2em' } }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,17 @@
-module.exports = {
+const withTW = require( '@bsf/force-ui/withTW' );
+
+module.exports = withTW( {
 	content: [
-		'./src/**/*.@(js|jsx)',
-		'./inc/blocks/**/*.php',
-		'./inc/fields/**/*.php',
-		'./templates/single-form.php',
-		'./inc/fields/**/*.php',
+		'./src/**/*.{js,jsx}',
 	],
-	theme: {
-		extend: {
-			colors: {
-				wpprimary: 'var(--wp-admin-theme-color)',
-				wpcolor: '#2271b1',
-				wphovercolor: '#135e96',
-				wphoverbgcolor: '#2271b117',
-				wpcolorfaded: '#2271b120',
-				srfm_primary_color: 'var(--srfm-primary-color)',
-				srfm_secondary_color: 'var(--srfm-secondary-color)',
-				srfm_primary_text_color: 'var(--srfm-primary-text-color)',
-				required_icon_color: '#EF4444',
-			},
-			fontFamily: {
-				inter: [ '"Inter"', 'sans-serif' ],
-			},
-			screens: {
-				'600px': { min: '600px', max: '781px' },
-			},
-		},
-	},
+	theme: {},
 	variants: {
 		extend: {
 			borderWidth: [ 'last' ],
 		},
 	},
 	plugins: [ require( '@tailwindcss/forms' ) ],
-	prefix: 'srfm-',
 	corePlugins: {
 		preflight: false,
 	},
-};
+});
