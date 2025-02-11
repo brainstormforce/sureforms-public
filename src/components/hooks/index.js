9e07abe058ffe7655554b966a77baf8ba75a33c7
Om Kolte <omk@bsf.io>
2025-02-11 17:36:44 +0530

    code refactor + lint js fixes

--- src/components/hooks/index.js
+++ src/components/hooks/index.js
@@ -16,13 +16,9 @@ export const formPresetPanel = ( panel, args ) => {
  * Filters the style panels by allowing additional panels or options to be added.
  *
  * @param {Array}  panels - The existing style panel configuration.
- * @param {Object} args    - Additional arguments for the filter.
+ * @param {Object} args   - Additional arguments for the filter.
  * @return {Array} - The filtered style panels.
  */
 export const getStylePanels = ( panels, args ) => {
-	return applyFilters(
-		'srfm.style.panels',
-		panels,
-		args
-	);
+	return applyFilters( 'srfm.style.panels', panels, args );
 };
