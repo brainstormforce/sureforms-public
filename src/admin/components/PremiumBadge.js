fe5f96b9852cea0ff3c79bc86d857bd01ebf2364
Om Kolte <omk@bsf.io>
2025-02-11 13:37:44 +0530

    using static svg for simplifying imports

--- src/admin/components/PremiumBadge.js
+++ src/admin/components/PremiumBadge.js
@@ -1,7 +1,5 @@
 import { __ } from '@wordpress/i18n';
 import { addQueryParam } from '../../utils/Helpers';
-import svgIcons from '@Svg/svgs.json';
-import parse from 'html-react-parser';
 
 export default ( { tooltipHeading = '', tooltipContent = '', tooltipPosition = 'bottom',
 	utmMedium = '',
@@ -17,7 +15,12 @@ export default ( { tooltipHeading = '', tooltipContent = '', tooltipPosition = '
 				}
 				}
 			>
-				<span className="srfm-premium-svg-wrapper">{ parse( svgIcons.lock_icon ) }</span>
+				<span className="srfm-premium-svg-wrapper">
+					<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M9.5 5.5H2.5C1.94772 5.5 1.5 5.94772 1.5 6.5V10C1.5 10.5523 1.94772 11 2.5 11H9.5C10.0523 11 10.5 10.5523 10.5 10V6.5C10.5 5.94772 10.0523 5.5 9.5 5.5Z" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+						<path d="M3.5 5.5V3.5C3.5 2.83696 3.76339 2.20107 4.23223 1.73223C4.70107 1.26339 5.33696 1 6 1C6.66304 1 7.29893 1.26339 7.76777 1.73223C8.23661 2.20107 8.5 2.83696 8.5 3.5V5.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+					</svg>
+				</span>
 				<span className="srfm-premium-text-wrapper"> { __( 'Premium', 'sureforms' ) } </span>
 			</span>
 			<div className={ `tooltip-wrap ${ tooltipPosition }` }>
