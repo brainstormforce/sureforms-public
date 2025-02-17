6a7931d560c9b248d7d718eb307c57a5fd40d145
ruz3n <ratna69420@gmail.com>
2025-02-17 10:51:57 +0545

    added create new form component with force ui

---
+++ src/admin/dashboard/AddNewForm.js
@@ -0,0 +1,156 @@
+/** @jsx jsx */
+import { __ } from '@wordpress/i18n';
+import { css, jsx } from '@emotion/react';
+import { Button, Container, Label } from '@bsf/force-ui';
+import { ArrowLeft, FilePlus, WandSparkles } from 'lucide-react';
+
+export default () => {
+	return (
+		<Container
+			className="p-4 gap-2 bg-background-primary border-0.5 border-solid border-border-subtle rounded-xl shadow-sm-blur-2"
+			containerType="flex"
+			direction="column"
+		>
+			<Container.Item
+				className="p-2 gap-6"
+			>
+				<Label
+					variant="neutral"
+					className="text-text-primary font-semibold text-2xl"
+				>
+					{ __( 'How would you like to create a new form?', 'sureforms' ) }
+				</Label>
+			</Container.Item>
+			<Container.Item
+				className="p-2 gap-6"
+			>
+				<Container
+					className=""
+					containerType="grid"
+					cols="12"
+				>
+					<Container.Item
+						colSpan="6"
+						className="bg-background-primary border-0.5 border-solid border-border-subtle rounded-lg shadow-sm-blur-2"
+					>
+						<Container
+							className=""
+							containerType="flex"
+							direction="column"
+						>
+							<Container.Item className="gap-2 p-3">
+								<Container
+									containerType="flex"
+									direction="column">
+									<Container.Item className="w-5">
+										<FilePlus size={ 20 } />
+									</Container.Item>
+									<Container.Item className="gap-1 p-1">
+										<Label
+											size="md"
+											variant="neutral"
+											className="font-medium text-text-primary text-lg"
+										>
+											{ __( 'Build Form From Scratch', 'sureforms' ) }
+										</Label>
+										<Label
+											size="sm"
+											variant="help"
+											className="text-text-tertiary font-normal text-xs"
+										>
+											{ __( 'Tailor your form precisely to your unique needs. No coding skills required—just unleash your creativity.', 'sureforms' ) }
+										</Label>
+									</Container.Item>
+								</Container>
+							</Container.Item>
+							<Container.Item className="gap-3 p-3">
+								<Button
+									className="gap-0.5 w-full bg-button-secondary border border-solid border-button-secondary shadow-sm-blur-2 hover:bg-button-secondary font-semibold rounded-lg text-button-text text-xs"
+									iconPosition="left"
+									size="md"
+									tag="button"
+									type="button"
+									variant="ghost"
+									onClick={ () => {
+										window.location.href = '#';
+									} }
+								>
+									{ __( 'Build From Sratch', 'sureforms' ) }
+								</Button>
+							</Container.Item>
+						</Container>
+					</Container.Item>
+					<Container.Item
+						colSpan="6"
+						className="bg-background-primary border-0.5 border-solid border-border-subtle rounded-lg shadow-sm-blur-2"
+					>
+						<Container
+							className=""
+							containerType="flex"
+							direction="column"
+						>
+							<Container.Item className="gap-2 p-3">
+								<Container
+									containerType="flex"
+									direction="column">
+									<Container.Item className="w-5">
+										<WandSparkles size={ 20 } />
+									</Container.Item>
+									<Container.Item className="gap-1 p-1">
+										<Label
+											size="md"
+											variant="neutral"
+											className="font-medium text-text-primary text-lg"
+										>
+											{ __( 'Generate Form with AI', 'sureforms' ) }
+										</Label>
+										<Label
+											size="sm"
+											variant="help"
+											className="text-text-tertiary font-normal text-xs"
+										>
+											{ __( 'Experience the future of form building with AI-powered forms. Use AI to build your form 10x faster.', 'sureforms' ) }
+										</Label>
+									</Container.Item>
+								</Container>
+							</Container.Item>
+							<Container.Item className="gap-3 p-3">
+								<Button
+									className="gap-0.5 w-full bg-button-primary border border-solid border-button-primary shadow-sm-blur-2 hover:bg-button-primary font-semibold rounded-lg text-button-text text-xs"
+									iconPosition="left"
+									size="md"
+									tag="button"
+									type="button"
+									variant="ghost"
+									onClick={ () => {
+										window.location.href = '#';
+									} }
+								>
+									{ __( 'Try the AI FormBuilder', 'sureforms' ) }
+								</Button>
+							</Container.Item>
+						</Container>
+					</Container.Item>
+				</Container>
+			</Container.Item >
+			<Container.Item
+				className="flex p-2 gap-6 justify-center"
+			>
+				<Button
+					className="text-text-tertiary font-semibold text-xs hover:cursor-pointer"
+					icon={ <ArrowLeft size={ 16 } /> }
+					iconPosition="left"
+					size="xs"
+					tag="button"
+					type="button"
+					variant="ghost"
+					onClick={ () => {
+						window.location.href = '#';
+					} }
+				>
+					{ __( 'Exit to Dashboard', 'sureforms' ) }
+				</Button>
+			</Container.Item >
+		</Container >
+	);
+};
