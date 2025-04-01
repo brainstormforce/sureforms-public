e73e69dee17cc03c8ceea88bd62a012ffea35bc1
ruz3n <ratna69420@gmail.com>
2025-04-01 16:34:18 +0545

    changes

--- admin/admin.php
+++ admin/admin.php
@@ -870,10 +870,12 @@ class Admin {
 
 	/**
 	 * Update Astra's menu priority to show after Dashboard menu.
+	 * @param int $menu_priority top level menu priority.
 	 *
 	 * @since x.x.x
 	 */
-	public function update_admin_menu_position() {
-		return 2.1;
+	public function update_admin_menu_position( $menu_priority ) {
+		$menu_priority = 2.1;
+		return $menu_priority;
 	}
 }
