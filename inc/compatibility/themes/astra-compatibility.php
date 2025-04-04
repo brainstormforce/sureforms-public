2669143ac23515d870469c8893e3670d83be16cb
ruz3n <ratna69420@gmail.com>
2025-04-04 16:21:02 +0545

    linting fixes

--- inc/compatibility/themes/astra-compatibility.php
+++ inc/compatibility/themes/astra-compatibility.php
@@ -3,7 +3,7 @@
  * Astra Compatibility.
  *
  * @package sureforms
- * @since x.x.x
+ * @since   x.x.x
  */
 
 namespace SRFM\Inc\Compatibility\Themes;
@@ -27,48 +27,48 @@ class Astra_Compatibility {
 	 * @since x.x.x
 	 */
 	public function __construct() {
-        // Update Astra's menu priority to show after Dashboard menu.
+		// Update Astra's menu priority to show after Dashboard menu.
 		add_filter( 'astra_menu_priority', [ $this, 'update_admin_menu_position' ] );
-    }
+	}
 
-    /**
-     * Update Astra's menu priority to show after Dashboard menu.
-     * Checks for existing menu items and assigns a new priority to Astra's menu item.
-     * Decreases the priority by 0.1 until it finds a unique value.
-     * If astra priority becomes less than dashboard priority, set it to 2 by default.
-     *
-     * @since x.x.x
-     */
-    public function update_admin_menu_position(): float {
-        global $menu;
+	/**
+	 * Update Astra's menu priority to show after Dashboard menu.
+	 * Checks for existing menu items and assigns a new priority to Astra's menu item.
+	 * Decreases the priority by 0.1 until it finds a unique value.
+	 * If astra priority becomes less than dashboard priority, set it to 2 by default.
+	 *
+	 * @since x.x.x
+	 */
+	public function update_admin_menu_position(): float {
+		global $menu;
 
-        $dashboard_priority = null;
+		$dashboard_priority = null;
 
-        foreach ( $menu as $position => $menu_item ) {
-            if ( isset( $menu_item[0] ) && 'Dashboard' === $menu_item[0] ) {
-                $dashboard_priority = (float) $position;
-                break;
-            }
-        }
+		foreach ( $menu as $position => $menu_item ) {
+			if ( isset( $menu_item[0] ) && 'Dashboard' === $menu_item[0] ) {
+				$dashboard_priority = (float) $position;
+				break;
+			}
+		}
 
-        if ( null === $dashboard_priority ) {
-            $dashboard_priority = 2.0;
-        }
+		if ( null === $dashboard_priority ) {
+			$dashboard_priority = 2.0;
+		}
 
-        $astra_priority = $dashboard_priority + 0.1;
+		$astra_priority = $dashboard_priority + 0.1;
 
-        $existing_priorities = array_keys( $menu );
+		$existing_priorities = array_keys( $menu );
 
-        while ( in_array( (string) $astra_priority, array_map( 'strval', $existing_priorities ), true ) ) {
-            $astra_priority -= 0.1;
-        }
+		while ( in_array( (string) $astra_priority, array_map( 'strval', $existing_priorities ), true ) ) {
+			$astra_priority -= 0.1;
+		}
 
-        if ( $astra_priority < $dashboard_priority ) {
-            $astra_priority = 2.0;
-        }
+		if ( $astra_priority < $dashboard_priority ) {
+			$astra_priority = 2.0;
+		}
 
-        $astra_priority = round( $astra_priority, 1 );
+		$astra_priority = round( $astra_priority, 1 );
 
-        return $astra_priority;
-    }
+		return $astra_priority;
+	}
 }
