1abc089e8aa3b36bec361f8c9f71d3f0dcfe7005
ruz3n <ratna69420@gmail.com>
2025-04-09 16:07:19 +0545

    linting fixes

--- plugin-loader.php
+++ plugin-loader.php
@@ -291,7 +291,7 @@ class Plugin_Loader {
 		AI_Auth::get_instance();
 		Updater::get_instance();
 		DatabaseRegister::init();
-		Astra::get_instance(); //Initializing Compatibilities
+		Astra::get_instance(); // Initializing Compatibilities.
 		/**
 		 * Required to add the if check for the class existence to resolve phpstan error,
 		 * as the phpstan configuration ignores the inc/lib directory which gives error
