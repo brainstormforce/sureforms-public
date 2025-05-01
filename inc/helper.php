c271484fdbe69076a888c7c2470c996d151290e2
Om Kolte <omk@bsf.io>
2025-05-01 16:45:38 +0530

    QA: Fixed Margin & Border Radius Issues

--- inc/helper.php
+++ inc/helper.php
@@ -747,7 +747,8 @@ class Helper {
 					'--srfm-input-field-padding'           => '10px 12px',
 					'--srfm-input-field-font-size'         => '14px',
 					'--srfm-input-field-line-height'       => '20px',
-					'--srfm-input-field-margin'            => '4px',
+					'--srfm-input-field-margin-top'            => '4px',
+					'--srfm-input-field-margin-bottom'            => '4px',
 					// Checkbox and GDPR Variables.
 					'--srfm-check-ctn-width'               => '16px',
 					'--srfm-check-ctn-height'              => '16px',
@@ -788,7 +789,8 @@ class Helper {
 					'--srfm-input-height'                  => '44px',
 					'--srfm-input-field-font-size'         => '16px',
 					'--srfm-input-field-line-height'       => '24px',
-					'--srfm-input-field-margin'            => '6px',
+					'--srfm-input-field-margin-top'            => '6px',
+					'--srfm-input-field-margin-bottom'            => '6px',
 					// Checkbox and GDPR Variables.
 					'--srfm-checkbox-margin-top-frontend'  => '4px',
 					'--srfm-checkbox-margin-top-editor'    => '6px',
@@ -829,7 +831,8 @@ class Helper {
 					'--srfm-input-field-padding'           => '10px 14px',
 					'--srfm-input-field-font-size'         => '18px',
 					'--srfm-input-field-line-height'       => '28px',
-					'--srfm-input-field-margin'            => '8px',
+					'--srfm-input-field-margin-top'            => '8px',
+					'--srfm-input-field-margin-bottom'            => '8px',
 					// Checkbox and GDPR Variables.
 					'--srfm-check-ctn-width'               => '20px',
 					'--srfm-check-ctn-height'              => '20px',
