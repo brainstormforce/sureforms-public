ad9e76bb1520a0662bdc142059f587e0dc598871
ruz3n <ratna69420@gmail.com>
2025-05-16 17:47:11 +0545

    issue with german language in add new form

--- src/admin/components/template-picker/ai-form-builder-components/AddNewForm.js
+++ src/admin/components/template-picker/ai-form-builder-components/AddNewForm.js
@@ -14,42 +14,34 @@ export default () => {
 		btnIcon,
 		variant,
 	} ) => (
-		<Container.Item className="flex flex-col gap-0 flex-1 border-0.5 border-solid border-border-subtle rounded-lg shadow-sm-blur-2">
-			<Container direction="column" className="gap-0">
-				<Container.Item>
-					<Container
-						direction="column"
-						className="gap-2 p-3"
-					>
-						<Container.Item>{ icon }</Container.Item>
-						<Container.Item className="flex flex-col gap-1 p-1">
-							<Title
-								size="sm"
-								title={ title }
-								className="font-medium"
-							/>
-							<Label size="sm" variant="help">
-								{ description }
-							</Label>
-						</Container.Item>
-					</Container>
-				</Container.Item>
-				<Container.Item>
-					<div className="border-t-0.5 border-b-0 border-y-0 border-solid border-border-subtle mt-auto"></div>
-				</Container.Item>
-				<Container.Item className="flex gap-3 mt-auto p-3">
-					<Button
-						className="w-full"
-						size="sm"
-						iconPosition={ iconPosition }
-						icon={ btnIcon }
-						variant={ variant }
-						onClick={ onClick }
-					>
-						{ buttonText }
-					</Button>
+		<Container.Item className="flex flex-col justify-between gap-0 flex-1 min-h-[260px] border-0.5 border-solid border-border-subtle rounded-lg shadow-sm-blur-2">
+			<Container
+				direction="column"
+				className="flex flex-col gap-0 p-3 flex-grow"
+			>
+				<Container.Item>{ icon }</Container.Item>
+				<Container.Item className="flex flex-col gap-1 p-1">
+					<Title size="sm" title={ title } className="font-medium" />
+					<Label size="sm" variant="help">
+						{ description }
+					</Label>
 				</Container.Item>
 			</Container>
+
+			<div className="border-t-0.5 border-b-0 border-y-0 border-solid border-border-subtle"></div>
+
+			<Container.Item className="flex gap-3 p-3">
+				<Button
+					className="w-full"
+					size="sm"
+					iconPosition={ iconPosition }
+					icon={ btnIcon }
+					variant={ variant }
+					onClick={ onClick }
+				>
+					{ buttonText }
+				</Button>
+			</Container.Item>
 		</Container.Item>
 	);
 
@@ -95,7 +87,11 @@ export default () => {
 	);
 
 	return (
-		<Container className="w-full h-screen p-8 gap-8" justify="center" align="center">
+		<Container
+			className="w-full h-screen p-8 gap-8"
+			justify="center"
+			align="center"
+		>
 			<Container
 				className="p-4 gap-2 bg-background-primary border-0.5 border-solid border-border-subtle rounded-xl shadow-sm-blur-2 max-w-[42.5rem]"
 				direction="column"
