502e4d7914f89612268d147f744074947bf8e11b
Om Kolte <omk@bsf.io>
2025-06-02 12:19:57 +0530

    optimize colors array implementation

--- assets/js/unminified/blocks/textarea.js
+++ assets/js/unminified/blocks/textarea.js
@@ -92,6 +92,43 @@ function addQuillEditor( areaField ) {
 	Quill.register( DirectionStyle, true ); // Register direction style
 
 	const quillId = `quill-${ getQuillId }`;
+	const colorsArray = [
+		'#000000',
+		'#e60000',
+		'#ff9900',
+		'#ffff00',
+		'#008a00',
+		'#0066cc',
+		'#9933ff',
+		'#ffffff',
+		'#facccc',
+		'#ffebcc',
+		'#ffffcc',
+		'#cce8cc',
+		'#cce0f5',
+		'#ebd6ff',
+		'#bbbbbb',
+		'#f06666',
+		'#ffc266',
+		'#ffff66',
+		'#66b966',
+		'#66a3e0',
+		'#c285ff',
+		'#888888',
+		'#a10000',
+		'#b26b00',
+		'#b2b200',
+		'#006100',
+		'#0047b2',
+		'#6b24b2',
+		'#444444',
+		'#5c0000',
+		'#663d00',
+		'#666600',
+		'#003700',
+		'#002966',
+		'#3d1466',
+	];
 
 	// Initialize Quill editor with a toolbar configuration
 	const quillEditor = new Quill( `#${ quillId }`, {
@@ -105,46 +142,8 @@ function addQuillEditor( areaField ) {
 					[ 'blockquote' ], // Blockquote
 					[ { align: [] } ], // Alignment (registered as inline style)
 					[
-						{
-							color: [
-								'#000000',
-								'#e60000',
-								'#ff9900',
-								'#ffff00',
-								'#008a00',
-								'#0066cc',
-								'#9933ff',
-								'#ffffff',
-								'#facccc',
-								'#ffebcc',
-								'#ffffcc',
-								'#cce8cc',
-								'#cce0f5',
-								'#ebd6ff',
-								'#bbbbbb',
-								'#f06666',
-								'#ffc266',
-								'#ffff66',
-								'#66b966',
-								'#66a3e0',
-								'#c285ff',
-								'#888888',
-								'#a10000',
-								'#b26b00',
-								'#b2b200',
-								'#006100',
-								'#0047b2',
-								'#6b24b2',
-								'#444444',
-								'#5c0000',
-								'#663d00',
-								'#666600',
-								'#003700',
-								'#002966',
-								'#3d1466',
-							],
-						},
-						{ background: [] },
+						{ color: colorsArray }, // Text color options.
+						{ background: colorsArray }, // Background color options.
 					], // Color and background
 					[ 'link' ], // Add links and images
 					[ 'clean' ], // Remove formatting
