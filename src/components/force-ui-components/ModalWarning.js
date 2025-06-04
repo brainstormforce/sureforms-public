ecdb4de431ffe63350fe64946797130d33b8b352
Rahul Verma <rahulvarma722@gmail.com>
2025-06-04 18:05:28 +0530

    resolved linting issue

--- src/components/force-ui-components/ModalWarning.js
+++ src/components/force-ui-components/ModalWarning.js
@@ -4,9 +4,7 @@ import { Container } from '@bsf/force-ui';
 
 const ModalWarning = ( { message } ) => {
 	return (
-		<Container
-			className="w-full p-3 gap-2 border border-solid border-alert-border-warning bg-alert-background-warning"
-		>
+		<Container className="w-full p-3 gap-2 border border-solid border-alert-border-warning bg-alert-background-warning">
 			<span className="size-5">{ parse( svg?.warning ) }</span>
 			<span className="text-sm font-normal">{ message }</span>
 		</Container>
