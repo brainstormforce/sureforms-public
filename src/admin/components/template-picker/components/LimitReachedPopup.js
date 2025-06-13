43fae924f743b76285a8d1a5bf360bce32ecd397
ruz3n <ratna69420@gmail.com>
2025-06-13 16:07:31 +0545

    requested changes

--- src/admin/components/template-picker/components/LimitReachedPopup.js
+++ src/admin/components/template-picker/components/LimitReachedPopup.js
@@ -18,68 +18,69 @@ const LimitReachedPopup = ( {
 		null
 	);
 
+	const renderLimitReachedContent = () => (
+		<Container
+			direction="column"
+			justify="center"
+			align="center"
+			className="fixed inset-0 bg-overlay-background z-[99999999]"
+		>
+			<Container
+				direction="column"
+				className="bg-background-primary gap-6 py-4 px-5 rounded-lg max-w-md shadow-lg"
+			>
+				<Container.Item className="relative pt-2">
+					<Label
+						variant="neutral"
+						className="text-lg font-bold flex gap-3"
+					>
+						<span className="pt-1">{ ICONS.warning }</span>
+						{ title }
+						<span
+							className="absolute top-[-10px] right-[-15px] cursor-pointer"
+							onClick={ () =>
+								( window.location.href =
+									srfm_admin.site_url +
+									'/wp-admin/admin.php?page=add-new-form' )
+							}
+						>
+							{ ICONS.close }
+						</span>
+					</Label>
+				</Container.Item>
+
+				<Container.Item className="flex flex-col gap-4">
+					<Label
+						size="md"
+						className="text-text-secondary font-normal"
+					>
+						{ paraOne }
+					</Label>
+					<Label
+						size="md"
+						className="text-text-secondary font-normal"
+					>
+						{ paraTwo }
+					</Label>
+				</Container.Item>
+
+				<Container.Item className="flex flex-col w-full gap-4 pb-2">
+					<Button
+						size="md"
+						variant="primary"
+						onClick={ onclick }
+					>
+						{ buttonText ?? __( 'Connect Now', 'sureforms' ) }
+					</Button>
+				</Container.Item>
+			</Container>
+		</Container>
+	);
+
 	return (
 		<>
 			<Header />
-			{ activateProPlugin ? (
-				activateLicensePopup
-			) : (
-				<Container
-					direction="column"
-					justify="center"
-					align="center"
-					className="fixed inset-0 bg-overlay-background z-[99999999]"
-				>
-					<Container
-						direction="column"
-						className="bg-background-primary gap-6 py-4 px-5 rounded-lg max-w-md shadow-lg"
-					>
-						<Container.Item className="relative pt-2">
-							<Label
-								variant="neutral"
-								className="text-lg font-bold flex gap-3"
-							>
-								<span className="pt-1">{ ICONS.warning }</span>
-								{ title }
-								<span
-									className="absolute top-[-10px] right-[-15px] cursor-pointer"
-									onClick={ () =>
-										( window.location.href =
-											srfm_admin.site_url +
-											'/wp-admin/admin.php?page=add-new-form' )
-									}
-								>
-									{ ICONS.close }
-								</span>
-							</Label>
-						</Container.Item>
-						<Container.Item className="flex flex-col gap-4">
-							<Label
-								size="md"
-								className="text-text-secondary font-normal"
-							>
-								{ paraOne }
-							</Label>
-							<Label
-								size="md"
-								className="text-text-secondary font-normal"
-							>
-								{ paraTwo }
-							</Label>
-						</Container.Item>
-						<Container.Item className="flex flex-col w-full gap-4 pb-2">
-							<Button
-								size="md"
-								variant="primary"
-								onClick={ onclick }
-							>
-								{ buttonText ??
-									__( 'Connect Now', 'sureforms' ) }
-							</Button>
-						</Container.Item>
-					</Container>
-				</Container>
-			) }
+			{ activateProPlugin ? activateLicensePopup : renderLimitReachedContent() }
 			<AiFormBuilder />
 		</>
 	);
