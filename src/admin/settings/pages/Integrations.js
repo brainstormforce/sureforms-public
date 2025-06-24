42029b163f28bc954ed7f2590a3800ad113ee31e
Om Kolte <omk@bsf.io>
2025-06-24 17:53:45 +0530

    addded filter for integrations page

--- src/admin/settings/pages/Integrations.js
+++ src/admin/settings/pages/Integrations.js
@@ -1,14 +1,30 @@
+import { applyFilters } from '@wordpress/hooks';
 import ContentCard from '../components/ContentCard';
 import SureTriggers from '../components/integrations/SureTriggers';
 import Webhooks from '../components/integrations/webhooks';
 import Zapier from '../components/integrations/zapier';
 
 const IntegrationsPage = ( { loading } ) => {
+	// Apply filter to allow Pro plugin to add additional integrations
+	const additionalContent = applyFilters(
+		'srfm.settings.integrations.additional.content',
+		[]
+	);
+
 	return (
 		<div className="flex flex-col gap-1 bg-background-secondary rounded-lg p-1">
 			<ContentCard loading={ loading } content={ <SureTriggers /> } />
 			<ContentCard loading={ loading } content={ <Webhooks /> } />
 			<ContentCard loading={ loading } content={ <Zapier /> } />
+
+			{/* Additional integrations from Pro plugin */}
+			{additionalContent.map((content, index) => (
+				<ContentCard 
+					key={`additional-integration-${index}`} 
+					loading={loading} 
+					content={content} 
+				/>
+			))}
 		</div>
 	);
 };
