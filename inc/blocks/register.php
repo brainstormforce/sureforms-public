36506dff1a84522458fab388e734d1a9f27ba240
Rahul Verma <rahulvarma722@gmail.com>
2025-07-04 17:31:24 +0530

    fix linting

--- inc/blocks/register.php
+++ inc/blocks/register.php
@@ -7,8 +7,8 @@
 
 namespace SRFM\Inc\Blocks;
 
-use SRFM\Inc\Traits\Get_Instance;
 use SRFM\Inc\Helper;
+use SRFM\Inc\Traits\Get_Instance;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
