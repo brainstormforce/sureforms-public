7418aee4c3812ffa7054d71d071392ff019bc172
Vansh Kapoor <kvansh297@gmail.com>
2025-07-29 10:38:38 +0530

    phpstan fix

--- inc/form-submit.php
+++ inc/form-submit.php
@@ -253,7 +253,8 @@ class Form_Submit {
 		$current_form_id = $form_data['form-id'];
 
 		// Check whether the form is valid.
-		if ( ! Helper::is_valid_form( $current_form_id ) ) {
+		$form_id = (int) $current_form_id;
+		if ( ! Helper::is_valid_form( $form_id ) ) {
 			wp_send_json_error(
 				[
 					'message' => __( 'Form does not exist. Please provide a valid form ID.', 'sureforms' ),
