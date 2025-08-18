dbe059eb078cf5c0fc2fc5bf0adcc7bd514245c0
ruz3n <ratna69420@gmail.com>
2025-08-18 16:50:28 +0545

    updated nps survey library

--- inc/lib/nps-survey/dist/main.asset.php
+++ inc/lib/nps-survey/dist/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '9a83e54041811ee7b09c');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '658616bdee8c2d4a582e');
