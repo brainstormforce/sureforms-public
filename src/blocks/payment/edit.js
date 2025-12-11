76e13dd01c4f0261ca4cf7f51bac6c7ef1a274f1
rahulv8085 <rahulvarma722@gmail.com>
2025-12-11 13:16:02 +0530

    resolving linting

--- src/blocks/payment/edit.js
+++ src/blocks/payment/edit.js
@@ -491,8 +491,8 @@ const Edit = ( props ) => {
 							title={ __( 'Attributes', 'sureforms' ) }
 							initialOpen={ true }
 						>
-							{ filterOptions.map(
-								( option ) => option?.component ? option.component : null
+							{ filterOptions.map( ( option ) =>
+								option?.component ? option.component : null
 							) }
 						</SRFMAdvancedPanelBody>
 					</InspectorTab>
