62b423689fff89e9eafe2e22afbd3f6bd90d975c
rahulv8085 <rahulvarma722@gmail.com>
2025-12-12 20:12:33 +0530

    Adding payment in front end

--- inc/frontend-assets.php
+++ inc/frontend-assets.php
@@ -308,6 +308,12 @@ class Frontend_Assets {
 				]
 			);
 		}
+
+		// Add action to enqueue payment provider scripts.
+		do_action( 'srfm_enqueue_block_scripts', [
+			'block_name' => $block_name,
+			'attr' => $attr,
+		] );
 	}
 
 	/**
