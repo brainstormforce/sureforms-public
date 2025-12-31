31339a7253141effeaf262f693077aed9ca83e34
ruz3n <ratna69420@gmail.com>
2025-12-31 14:17:10 +0545

    minor changes

--- inc/field-validation.php
+++ inc/field-validation.php
@@ -166,7 +166,6 @@ class Field_Validation {
 				if ( isset( $block['blockName'] ) ) {
 					// 'name_with_id' is used as a unique field identifier for validation.
 					// Example: 'sureforms-input-abc123' for blockName 'sureforms/input' and block_id 'abc123'
-					// $get_form_config[ $index ]['name_with_id'] = str_replace( '/', '-', $block['blockName'] ) . '-' . $index;
 					$name_with_id = str_replace( '/', '-', $block['blockName'] ) . '-' . $index;
 
 					// Allow custom filter based on block type.
