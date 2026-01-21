aa053a28674d70d60fdffdbad65daab79232e51f
ruz3n <ratna69420@gmail.com>
2026-01-21 17:26:21 +0545

    added alignment for single form settigs header

--- src/admin/single-form-settings/components/dialog/Dialog.js
+++ src/admin/single-form-settings/components/dialog/Dialog.js
@@ -536,16 +536,20 @@ const Dialog = ( {
 						gap="none"
 						className="w-full h-full pt-3 divide-y divide-x-0 divide-solid divide-border-subtle"
 					>
-						<Container className="py-2 px-4" justify="between">
+						<Container
+							className="py-2 px-4 align-start"
+							justify="between"
+						>
 							<Title
 								tag="h6"
 								title={ __( 'Form Behavior', 'sureforms' ) }
 								size="xs"
+								className="leading-none m-0"
 							/>
 							<Button
 								variant="ghost"
 								size="sm"
-								className="p-1"
+								className="p-1 self-start"
 								onClick={ close }
 								icon={ <XIcon /> }
 							/>
