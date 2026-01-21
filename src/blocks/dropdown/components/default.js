c9e1b0ed2cabe4e35e104d6a925c14a2f23f4906
Om Kolte <omk@bsf.io>
2026-01-21 17:17:23 +0530

    simplified dropdown filtered view implementation

--- src/blocks/dropdown/components/default.js
+++ src/blocks/dropdown/components/default.js
@@ -20,30 +20,6 @@ export const DropdownComponent = ( { attributes, setAttributes, blockID } ) => {
 		attributes
 	);
 
-	if ( filteredView ) {
-		return (
-			<>
-				<RichText
-					tagName="label"
-					value={ label }
-					onChange={ ( value ) => {
-						setAttributes( { label: decodeHtmlEntities( value ) } );
-					} }
-					className={ `srfm-block-label${ isRequired }` }
-					multiline={ false }
-					id={ `srfm-listbox-label ${ blockID }` }
-					allowedFormats={ [] }
-				/>
-				<HelpText
-					help={ help }
-					setAttributes={ setAttributes }
-					block_id={ blockID }
-				/>
-				{ filteredView }
-			</>
-		);
-	}
-
 	return (
 		<>
 			<RichText
@@ -62,34 +38,38 @@ export const DropdownComponent = ( { attributes, setAttributes, blockID } ) => {
 				setAttributes={ setAttributes }
 				block_id={ blockID }
 			/>
-			<div className={ `srfm-block-wrap srfm-dropdown-common-wrap` }>
-				<input
-					type="text"
-					className={ `srfm-input-common srfm-${ slug }-input` }
-					id={ `srfm-${ slug }-state-${ blockID }` }
-					aria-required={ required ? 'true' : 'false' }
-					placeholder={ placeholderText }
-					readOnly
-				/>
-				<div className="ts-dropdown-icon">
-					<svg
-						width="16"
-						height="16"
-						viewBox="0 0 16 16"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M4 6L8 10L12 6"
-							stroke="currentColor"
-							strokeOpacity="0.65"
-							strokeWidth="1.25"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-						/>
-					</svg>
+			{ filteredView ? (
+				filteredView
+			) : (
+				<div className={ `srfm-block-wrap srfm-dropdown-common-wrap` }>
+					<input
+						type="text"
+						className={ `srfm-input-common srfm-${ slug }-input` }
+						id={ `srfm-${ slug }-state-${ blockID }` }
+						aria-required={ required ? 'true' : 'false' }
+						placeholder={ placeholderText }
+						readOnly
+					/>
+					<div className="ts-dropdown-icon">
+						<svg
+							width="16"
+							height="16"
+							viewBox="0 0 16 16"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M4 6L8 10L12 6"
+								stroke="currentColor"
+								strokeOpacity="0.65"
+								strokeWidth="1.25"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+						</svg>
+					</div>
 				</div>
-			</div>
+			) }
 		</>
 	);
 };
