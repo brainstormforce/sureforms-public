c390dc972f794527bacb968ae198ede330347714
Vansh Kapoor <kvansh297@gmail.com>
2026-04-06 21:15:35 +0530

    fix comments

--- src/admin/onboarding/pages/premium-features.js
+++ src/admin/onboarding/pages/premium-features.js
@@ -484,8 +484,9 @@ const PremiumFeatures = () => {
 
 	// Get the display name for the plan
 	const getPlanDisplayName = ( _plan ) => {
+		/* translators: %s: plan name (Starter, Pro, Business) */
 		/* translators: %s: plan name */
-		return sprintf( __( 'SureForms %s', 'sureforms' ), plan );
+		return sprintf( __( 'SureForms %s', 'sureforms' ), __( 'Business', 'sureforms' ) );
 	};
 
 	return (
